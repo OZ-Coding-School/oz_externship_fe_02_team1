541bae1308281cb3dbfe97dc618afc7c4c889b21
gunysunny <mikan31@naver.com>
2025-09-02 11:32:10 +0900

    ♻️ refactor: common/form/index.ts를 default export 방식으로 변경 (#7)

---
+++ src/components/common/form/Index.ts
@@ -0,0 +1,4 @@
+export { default as Input } from './Input'
+export { default as TimeInput } from './TimeInput'
+export { default as Textarea } from './Textarea'
+export { default as DateInput } from './DateInput'
--- src/components/form/Index.ts
+++
@@ -1,4 +0,0 @@
-export { Input } from './Input'
-export { Textarea } from './Textarea'
-export { DateInput } from './DateInput'
-export { TimeInput } from './TimeInput'
