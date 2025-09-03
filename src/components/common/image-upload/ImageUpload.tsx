daa5057f62b6040a66541ef3c853d64de8a0841d
minji_105 <ajhjmj9@naver.com>
2025-09-03 11:47:07 +0900

    ✨ Feat: 이미지 삭제/변경 기능 추가 (#20)

--- src/components/common/image-upload/ImageUpload.tsx
+++ src/components/common/image-upload/ImageUpload.tsx
@@ -1,12 +1,13 @@
-import React, { useState } from 'react'
-import { Text } from '@/components'
+import React, { useRef, useState } from 'react'
+import { Button, Text } from '@/components'
 import { ImageUploadIcon } from '@/assets'
 
 export default function ImageUpload() {
   const [image, setImage] = useState<string | null>(null)
   const [imageName, setImageName] = useState<string | null>(null)
+  const fileInputRef = useRef<HTMLInputElement>(null)
 
-  const readImage = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleImageRead = (e: React.ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0]
     if (!file) return
 
@@ -22,28 +23,60 @@ export default function ImageUpload() {
     setImageName(file.name)
   }
 
+  const handleImageDelete = () => {
+    setImage(null)
+    setImageName(null)
+
+    if (fileInputRef.current) {
+      fileInputRef.current.value = ''
+    }
+  }
+
+  const handleImageChange = () => {
+    if (fileInputRef.current) {
+      fileInputRef.current.click()
+    }
+  }
+
   return (
     <div className="flex flex-col items-center justify-center rounded-lg border-2 border-dashed border-gray-300 p-6.5">
-      {image && imageName && <img src={image} alt={imageName} />}
-
-      <label
-        htmlFor="file-upload"
-        className="flex w-full cursor-pointer flex-col items-center justify-center"
-      >
-        <ImageUploadIcon />
-        <Text className="text-gray-600" variant="small">
-          클릭하여 이미지 업로드
-        </Text>
-        <Text className="mt-1 text-gray-400" variant="extraSmall">
-          JPG, PNG (최대 5MB)
-        </Text>
-        <input
-          id="file-upload"
-          type="file"
-          onChange={readImage}
-          className="hidden"
-        />
-      </label>
+      {image && imageName ? (
+        <>
+          <img src={image} alt={imageName} />
+          <div className="mt-4 flex gap-4">
+            <Button
+              variant="secondary"
+              size="small"
+              onClick={handleImageDelete}
+            >
+              이미지 삭제
+            </Button>
+            <Button size="small" onClick={handleImageChange}>
+              이미지 변경
+            </Button>
+          </div>
+        </>
+      ) : (
+        <label
+          htmlFor="file-upload"
+          className="flex w-full cursor-pointer flex-col items-center justify-center"
+        >
+          <ImageUploadIcon />
+          <Text className="text-gray-600" variant="small">
+            클릭하여 이미지 업로드
+          </Text>
+          <Text className="mt-1 text-gray-400" variant="extraSmall">
+            JPG, PNG (최대 5MB)
+          </Text>
+        </label>
+      )}
+      <input
+        id="file-upload"
+        type="file"
+        onChange={handleImageRead}
+        className="hidden"
+        ref={fileInputRef}
+      />
     </div>
   )
 }
