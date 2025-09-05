90ca8a35a5ad42bd94a6e117acb735eebcd16076
Jun <nbc7799@naver.com>
2025-09-05 12:10:56 +0900

    ✨ Feature: Toast 컴포넌트 (#32)

--- src/components/common/toast/Toast.tsx
+++ src/components/common/toast/Toast.tsx
@@ -1,42 +1,8 @@
 import { cn } from '@/utils'
-import { Text } from '../text'
-import {
-  CheckCircleIcon,
-  ExclamationCircleIcon,
-  XCircleIcon,
-  XMarkIcon,
-} from '@heroicons/react/24/outline'
-
-type ToastType = 'success' | 'warning' | 'error'
-
-interface ToastProps {
-  type: ToastType
-  title: string
-  message: string
-  onClose?: () => void
-  className?: string
-}
-
-const typeStyles = {
-  success: {
-    bg: 'bg-green-50',
-    text: 'text-green-800',
-    icon: CheckCircleIcon,
-    border: 'border-green-200',
-  },
-  warning: {
-    bg: 'bg-yellow-50',
-    text: 'text-yellow-400',
-    icon: ExclamationCircleIcon,
-    border: 'border-yellow-200',
-  },
-  error: {
-    bg: 'bg-red-50',
-    text: 'text-red-800',
-    icon: XCircleIcon,
-    border: 'border-red-200',
-  },
-}
+import Text from '@/components/common/text/Text'
+import { XMarkIcon } from '@heroicons/react/24/outline'
+import { toastStyles } from '@/components/common/toast/Toast.styles'
+import type { ToastProps } from '@/components/common/toast/toast.types'
 
 export default function Toast({
   type,
@@ -45,17 +11,16 @@ export default function Toast({
   onClose,
   className,
 }: ToastProps) {
-  const Icon = typeStyles[type].icon
-  const styles = typeStyles[type]
+  const { icon: Icon, bg, text, border } = toastStyles[type]
 
   return (
     <div
       className={cn(
         'border-offset-[-1px] flex w-96 max-w-96 justify-between gap-3 rounded-lg border-1 p-4 leading-tight font-medium',
         className,
-        styles.bg,
-        styles.text,
-        styles.border
+        bg,
+        text,
+        border
       )}
     >
       <Icon className="h-5 w-5 stroke-1" />
