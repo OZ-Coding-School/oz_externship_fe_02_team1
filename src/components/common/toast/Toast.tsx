23636edd6ed2d365e9e79d8c6e157555bdff7114
Jun <nbc7799@naver.com>
2025-09-05 11:35:23 +0900

    ✨ Feature: toast 알림 구현 (#32)

--- src/components/common/toast/Toast.tsx
+++ src/components/common/toast/Toast.tsx
@@ -1,10 +1,72 @@
+import { cn } from '@/utils'
 import { Text } from '../text'
+import {
+  CheckCircleIcon,
+  ExclamationCircleIcon,
+  XCircleIcon,
+  XMarkIcon,
+} from '@heroicons/react/24/outline'
 
-export default function Toast() {
+type ToastType = 'success' | 'warning' | 'error'
+
+interface ToastProps {
+  type: ToastType
+  title: string
+  message: string
+  onClose?: () => void
+  className?: string
+}
+
+const typeStyles = {
+  success: {
+    bg: 'bg-green-50',
+    text: 'text-green-800',
+    icon: CheckCircleIcon,
+    border: 'border-green-200',
+  },
+  warning: {
+    bg: 'bg-yellow-50',
+    text: 'text-yellow-400',
+    icon: ExclamationCircleIcon,
+    border: 'border-yellow-200',
+  },
+  error: {
+    bg: 'bg-red-50',
+    text: 'text-red-800',
+    icon: XCircleIcon,
+    border: 'border-red-200',
+  },
+}
+
+export default function Toast({
+  type,
+  title,
+  message,
+  onClose,
+  className,
+}: ToastProps) {
+  const Icon = typeStyles[type].icon
+  const styles = typeStyles[type]
+
   return (
-    <div className="flex h-20 w-96 max-w-96 flex-col rounded-lg bg-green-50 p-4.25 leading-tight font-medium text-green-800 outline-1 outline-offset-[-1px] outline-green-200">
-      <Text variant="small">성공적으로 저장되었습니다</Text>
-      <Text variant="small">변경사항이 성공적으로 적용되었습니다.</Text>
+    <div
+      className={cn(
+        'border-offset-[-1px] flex w-96 max-w-96 justify-between gap-3 rounded-lg border-1 p-4 leading-tight font-medium',
+        className,
+        styles.bg,
+        styles.text,
+        styles.border
+      )}
+    >
+      <Icon className="h-5 w-5 stroke-1" />
+      <div className="flex flex-1 flex-col">
+        <Text variant="small">{title}</Text>
+        <Text variant="small">{message}</Text>
+      </div>
+      <XMarkIcon
+        onClick={onClose}
+        className="h-5 w-5 cursor-pointer stroke-1"
+      />
     </div>
   )
 }
