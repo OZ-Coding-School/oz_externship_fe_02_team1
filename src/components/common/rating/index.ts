ebed1d3a159bc47b8382ccc0e4424aee7b9e0dd6
gunysunny <mikan31@naver.com>
2025-09-05 16:16:58 +0900

    ✨Feature: rating/index.ts에서 rating 폴더 내부 파일들 배럴 연결 (#34)

---
+++ src/components/common/rating/index.ts
@@ -0,0 +1,11 @@
+export { default as Rating } from '@/components/common/rating/Rating'
+export { default as ratingUtils } from '@/components/common/rating/rating.utils'
+export { default as useRatingInteractions } from '@/components/common/rating/rating.interactions'
+
+export type {
+  RatingCommon,
+  RatingInteractiveProps,
+  RatingReadOnlyProps,
+  RatingProps,
+  UseRatingInteractionsOptions,
+} from '@/components/common/rating/rating.types'
