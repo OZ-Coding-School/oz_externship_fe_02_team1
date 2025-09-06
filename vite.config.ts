bc671a750f8a5d69b4390e2465a7593cdeedfa50
minji_105 <ajhjmj9@naver.com>
2025-09-06 19:47:22 +0900

    🔧 Chore: typescript 및 vite에 alias 경로 추가 (#50)

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,15 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
+      '@api': path.resolve(__dirname, './src/api'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@hooks': path.resolve(__dirname, './src/hooks'),
+      '@lib': path.resolve(__dirname, './src/lib'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@store': path.resolve(__dirname, './src/store'),
+      '@types': path.resolve(__dirname, './src/types'),
+      '@utils': path.resolve(__dirname, './src/utils'),
     },
   },
 })
