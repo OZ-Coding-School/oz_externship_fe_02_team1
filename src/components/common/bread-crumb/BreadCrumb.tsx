b6165f652b0aee5b7bf48872469e9feae35c86d6
Jun <nbc7799@naver.com>
2025-09-06 20:50:42 +0900

    ✨ Feature: 기본로직구현 (#51)

--- src/components/common/bread-crumb/BreadCrumb.tsx
+++ src/components/common/bread-crumb/BreadCrumb.tsx
@@ -1,3 +1,49 @@
-export default function BreadCrumb() {
-  return <div>BreadCrumb</div>
+import { ChevronRightIcon } from '@heroicons/react/24/outline'
+import { Link } from 'react-router'
+
+interface BreadCrumbLinkItem {
+  label: string
+  to: string | undefined
+}
+
+interface BreadCrumbCurrentItem {
+  label: string
+  to?: never
+}
+
+type BreadCrumbItem = BreadCrumbLinkItem | BreadCrumbCurrentItem
+
+interface BreadCrumbProps {
+  items: BreadCrumbItem[]
+}
+
+export default function BreadCrumb({ items }: BreadCrumbProps) {
+  return (
+    <nav aria-label="breadCrumb" className="p-[25px] text-sm text-gray-600">
+      <ol className="flex">
+        {items.map((item, i) => (
+          <li
+            key={'to' in item ? item.to : item.label}
+            className="flex items-center gap-2"
+          >
+            {'to' in item ? (
+              <Link to={item.to as string} className="pl-2">
+                {item.label}
+              </Link>
+            ) : (
+              <span
+                className="text-foreground pl-2 font-medium text-gray-900"
+                aria-current="page"
+              >
+                {item.label}
+              </span>
+            )}
+            {i < items.length - 1 && (
+              <ChevronRightIcon className="text-muted-foreground h-4 w-4 pl-2" />
+            )}
+          </li>
+        ))}
+      </ol>
+    </nav>
+  )
 }
