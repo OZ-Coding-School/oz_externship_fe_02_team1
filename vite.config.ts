769227d3e78097121b59991f80b4f382da177505
minji_105 <ajhjmj9@naver.com>
2025-09-06 20:42:48 +0900

    🔧 Chore: alias 경로 index.ts 파일로 연결 (#50)

--- vite.config.ts
+++ vite.config.ts
@@ -11,14 +11,14 @@ export default defineConfig({
     alias: {
       '@': path.resolve(__dirname, './src'),
       '@api': path.resolve(__dirname, './src/api'),
-      '@assets': path.resolve(__dirname, './src/assets'),
-      '@components': path.resolve(__dirname, './src/components'),
+      '@assets': path.resolve(__dirname, './src/assets/index.ts'),
+      '@components': path.resolve(__dirname, './src/components/index.ts'),
       '@hooks': path.resolve(__dirname, './src/hooks'),
       '@lib': path.resolve(__dirname, './src/lib'),
       '@pages': path.resolve(__dirname, './src/pages'),
       '@store': path.resolve(__dirname, './src/store'),
       '@types': path.resolve(__dirname, './src/types'),
-      '@utils': path.resolve(__dirname, './src/utils'),
+      '@utils': path.resolve(__dirname, './src/utils/index.ts'),
     },
   },
 })
