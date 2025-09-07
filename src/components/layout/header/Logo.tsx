84e300bfad8494f46830eb1c8d0db6191b4d3a17
minji_105 <ajhjmj9@naver.com>
2025-09-08 02:12:43 +0900

    ✨ Feature: Logo 컴포넌트 추가 (#36)

---
+++ src/components/layout/header/Logo.tsx
@@ -0,0 +1,9 @@
+import { Text } from '@components'
+
+export default function Logo() {
+  return (
+    <div className="bg-primary-500 flex h-8 w-8 items-center justify-center rounded-lg">
+      <Text className="font-bold text-white">S</Text>
+    </div>
+  )
+}
