02bcc712ae9bb2a44b7106fef05edec4070d299c
Jun <nbc7799@naver.com>
2025-09-08 00:28:59 +0900

    ✨ Feature: index에 card export 추가 (#59)

---
+++ src/components/common/card/Card.tsx
@@ -0,0 +1,32 @@
+import { cn } from '@utils'
+import { Text } from '@components'
+import type { JSX } from 'react'
+
+interface CardProps {
+  title: string
+  content: JSX.Element
+  titleVariant: 'base' | 'large'
+  className?: string
+}
+
+export default function Card({
+  title,
+  content,
+  titleVariant = 'large',
+  className,
+}: CardProps) {
+  return (
+    <div className="flex flex-col rounded-lg border border-gray-200 bg-white p-4">
+      <Text
+        variant={titleVariant}
+        className={cn(
+          'pb-2 leading-normal font-semibold text-black',
+          className
+        )}
+      >
+        {title}
+      </Text>
+      {content}
+    </div>
+  )
+}
