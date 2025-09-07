dfe40cafb71de36c1430ac51827402bc8591b896
minji_105 <ajhjmj9@naver.com>
2025-09-08 03:50:15 +0900

    ♻️ Refactor: Header에 mediaQuery 적용 및 데스크탑 헤더 파일 분리 (#36)

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -1,45 +1,17 @@
-import { Avatar, Button, Text, Logo } from '@components'
-import { Link } from 'react-router'
-import { BellIcon } from '@heroicons/react/24/outline'
-
-const HEADER_NAV_LISTS = ['강의 목록', '스터디 그룹', '구인 공고']
+import { useMediaQuery } from 'react-responsive'
+import { mediaQuery } from '@constants'
+import { HeaderDesktop } from '@components'
 
 interface HeaderProps {
   isLoggedin: boolean
 }
 
 export default function Header({ isLoggedin }: HeaderProps) {
+  const isMobile = useMediaQuery({ query: mediaQuery.mobile })
+
   return (
     <header className="flex h-16 items-center px-20">
-      <div className="flex w-full max-w-7xl justify-between px-8">
-        <Link to="/" className="flex items-center gap-2">
-          <Logo />
-          <Text className="text-primary-600 text-xl font-bold">StudyHub</Text>
-        </Link>
-        <div className="flex items-center">
-          <nav>
-            <ul className="flex gap-8">
-              {HEADER_NAV_LISTS.map((el) => (
-                <li>{el}</li>
-              ))}
-            </ul>
-          </nav>
-          {isLoggedin ? (
-            <div className="ml-8 flex items-center">
-              <BellIcon height="20" />
-              <Avatar src="none" alt="김개발" size="sm" className="mr-2 ml-4" />
-              <Text className="text-primary-600">김개발</Text>
-            </div>
-          ) : (
-            <>
-              <Button variant="ghost" className="text-base">
-                로그인
-              </Button>
-              <Button className="ml-0 text-base">회원가입</Button>
-            </>
-          )}
-        </div>
-      </div>
+      {isMobile ? <></> : <HeaderDesktop isLoggedin={isLoggedin} />}
     </header>
   )
 }
---
+++ src/components/layout/header/HeaderDesktop.tsx
@@ -0,0 +1,43 @@
+import { Avatar, Button, Text, Logo } from '@components'
+import { Link } from 'react-router'
+import { BellIcon } from '@heroicons/react/24/outline'
+
+const HEADER_NAV_LISTS = ['강의 목록', '스터디 그룹', '구인 공고']
+
+interface HeaderDesktopProps {
+  isLoggedin: boolean
+}
+
+export default function HeaderDesktop({ isLoggedin }: HeaderDesktopProps) {
+  return (
+    <div className="flex w-full max-w-7xl justify-between px-8">
+      <Link to="/" className="flex items-center gap-2">
+        <Logo />
+        <Text className="text-primary-600 text-xl font-bold">StudyHub</Text>
+      </Link>
+      <div className="flex items-center">
+        <nav>
+          <ul className="flex gap-8">
+            {HEADER_NAV_LISTS.map((el) => (
+              <li>{el}</li>
+            ))}
+          </ul>
+        </nav>
+        {isLoggedin ? (
+          <div className="ml-8 flex items-center">
+            <BellIcon height="20" />
+            <Avatar src="none" alt="김개발" size="sm" className="mr-2 ml-4" />
+            <Text className="text-primary-600">김개발</Text>
+          </div>
+        ) : (
+          <>
+            <Button variant="ghost" className="text-base">
+              로그인
+            </Button>
+            <Button className="ml-0 text-base">회원가입</Button>
+          </>
+        )}
+      </div>
+    </div>
+  )
+}
