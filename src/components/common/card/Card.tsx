e3ae985720c7b7ec245379bb0ff5844f7f8ec78b
Jun <nbc7799@naver.com>
2025-09-08 13:28:45 +0900

    ✨ Feature: 기본 카드 컴포넌트 제작 (#59)

--- src/components/common/card/Card.tsx
+++ src/components/common/card/Card.tsx
@@ -1,3 +1,33 @@
-export default function Card() {
-  return <div>Card</div>
+import type { JSX } from 'react'
+
+import { Text } from '@components'
+import { cn } from '@utils'
+
+interface CardProps {
+  title: string
+  content: JSX.Element
+  titleVariant: 'base' | 'large'
+  className?: string
+}
+
+export default function Card({
+  title,
+  content,
+  titleVariant = 'large',
+  className,
+}: CardProps) {
+  return (
+    <div className="flex flex-col rounded-lg border border-gray-200 bg-white p-4">
+      <Text
+        variant={titleVariant}
+        className={cn(
+          'pb-2 leading-normal font-semibold text-black',
+          className
+        )}
+      >
+        {title}
+      </Text>
+      {content}
+    </div>
+  )
 }
