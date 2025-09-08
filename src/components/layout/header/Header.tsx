5149c494e04eadef77db93c51477c7360e7f7e01
minji_105 <ajhjmj9@naver.com>
2025-09-08 11:57:44 +0900

    ✨ Feature: HeaderMobil 컴포넌트 추가 및 헤더 조건부 렌더링 (#36)

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -1,6 +1,6 @@
 import { useMediaQuery } from 'react-responsive'
 import { mediaQuery } from '@constants'
-import { HeaderDesktop } from '@components'
+import { HeaderDesktop, HeaderMobile } from '@components'
 
 interface HeaderProps {
   isLoggedin: boolean
@@ -10,8 +10,12 @@ export default function Header({ isLoggedin }: HeaderProps) {
   const isMobile = useMediaQuery({ query: mediaQuery.mobile })
 
   return (
-    <header className="flex h-16 items-center px-20">
-      {isMobile ? <></> : <HeaderDesktop isLoggedin={isLoggedin} />}
+    <header className="flex h-16 items-center px-4 sm:px-20">
+      {isMobile ? (
+        <HeaderMobile isLoggedin={isLoggedin} />
+      ) : (
+        <HeaderDesktop isLoggedin={isLoggedin} />
+      )}
     </header>
   )
 }
