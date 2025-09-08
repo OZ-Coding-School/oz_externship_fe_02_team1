b3690a0a1c9039764d9fdb8735cd71b40dba33ae
gunysunny <mikan31@naver.com>
2025-09-08 13:30:02 +0900

    ♻️ refactor: barrel에 group/date-picker export 추가 (#49)

---
+++ src/components/common/group/index.ts
@@ -0,0 +1,4 @@
+export { default as BasicInfoSection } from '@/components/common/group/BasicInfoSection'
+export { default as PeriodMembersSection } from '@/components/common/group/PeriodMembersSection'
+export { default as LecturePickerSection } from '@/components/common/group/LecturePickerSection'
+export { default as DateModal } from '@/components/common/group/DatePickerModal'
