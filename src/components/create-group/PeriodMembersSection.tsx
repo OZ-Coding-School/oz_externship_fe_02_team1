310281ceef308dcbec18f972f8f0803e5e0eb99d
gunysunny <mikan31@naver.com>
2025-09-09 15:16:03 +0900

    ♻️ Refactor: PeriodMembersSection 중복 제거·코드 간소화 및 타입 정리 (#49)

--- src/components/create-group/PeriodMembersSection.tsx
+++ src/components/create-group/PeriodMembersSection.tsx
@@ -1,8 +1,8 @@
-import { cn } from '@/utils'
-import DateInput from '@/components/common/form/DateInput'
-import { MIN_MEMBERS, MAX_MEMBERS } from './constants'
+import { DateRangeFields } from '@components'
+import { MembersSlider } from '@components'
+import { H2 } from '@/components'
 
-type Props = {
+interface PeriodMembersSectionProps {
   startDate: string
   endDate: string
   maxMembers: number
@@ -20,74 +20,21 @@ const PeriodMembersSection = ({
   onOpenStart,
   onOpenEnd,
   onChangeMaxMembers,
-}: Props) => {
+}: PeriodMembersSectionProps) => {
   return (
     <section className="mt-6 rounded-xl border border-gray-200 bg-white p-6 shadow-sm">
-      <h2 className="mb-4 text-base font-semibold text-gray-900">
-        스터디 기간 및 인원
-      </h2>
+      <H2 className="mb-4">스터디 기간 및 인원</H2>
 
-      <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
-        <div className="relative">
-          <DateInput
-            label="스터디 시작일 *"
-            value={startDate}
-            onOpenCalendar={onOpenStart}
-            placeholder="날짜를 선택하세요"
-            aria-label="스터디 시작일"
-          />
-        </div>
+      <DateRangeFields
+        startDate={startDate}
+        endDate={endDate}
+        hasRangeError={hasRangeError}
+        openStartPicker={onOpenStart}
+        openEndPicker={onOpenEnd}
+      />
 
-        <div className="relative">
-          <DateInput
-            label="스터디 종료일 *"
-            value={endDate}
-            onOpenCalendar={onOpenEnd}
-            placeholder="날짜를 선택하세요"
-            invalid={hasRangeError}
-            errorText={
-              hasRangeError
-                ? '종료일은 시작일로부터 최소 5일 이후여야 합니다.'
-                : undefined
-            }
-            aria-label="스터디 종료일"
-          />
-        </div>
-      </div>
-
-      <div className="mt-6">
-        <div className="mb-2 flex items-center justify-between">
-          <label htmlFor="maxMembers" className="text-sm text-gray-700">
-            최대 인원 수 *
-          </label>
-          <span className="text-sm text-gray-600">{maxMembers} 명</span>
-        </div>
-
-        <input
-          id="maxMembers"
-          type="range"
-          min={MIN_MEMBERS}
-          max={MAX_MEMBERS}
-          step={1}
-          value={maxMembers}
-          onChange={(e) => onChangeMaxMembers(e.currentTarget.valueAsNumber)}
-          aria-valuemin={MIN_MEMBERS}
-          aria-valuemax={MAX_MEMBERS}
-          aria-valuenow={maxMembers}
-          className={cn(
-            'w-full appearance-none focus:outline-none',
-            '[&::-webkit-slider-runnable-track]:h-2 [&::-webkit-slider-runnable-track]:rounded-full [&::-webkit-slider-runnable-track]:bg-gray-200',
-            '[&::-moz-range-track]:h-2 [&::-moz-range-track]:rounded-full [&::-moz-range-track]:bg-gray-200',
-            '[&::-webkit-slider-thumb]:bg-primary-500 [&::-webkit-slider-thumb]:mt-[-6px] [&::-webkit-slider-thumb]:h-5 [&::-webkit-slider-thumb]:w-5 [&::-webkit-slider-thumb]:appearance-none [&::-webkit-slider-thumb]:rounded-full [&::-webkit-slider-thumb]:border-2 [&::-webkit-slider-thumb]:border-white [&::-webkit-slider-thumb]:shadow',
-            '[&::-moz-range-thumb]:bg-primary-500 [&::-moz-range-thumb]:h-5 [&::-moz-range-thumb]:w-5 [&::-moz-range-thumb]:rounded-full [&::-moz-range-thumb]:border-2 [&::-moz-range-thumb]:border-white [&::-moz-range-thumb]:shadow'
-          )}
-        />
-
-        <div className="mt-1 flex justify-between text-[12px] leading-5 text-gray-400">
-          <span>{MIN_MEMBERS}명</span>
-          <span>{MAX_MEMBERS}명</span>
-        </div>
-      </div>
+      {/* 인원 슬라이더 */}
+      <MembersSlider value={maxMembers} onChange={onChangeMaxMembers} />
     </section>
   )
 }
