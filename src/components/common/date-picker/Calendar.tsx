e5b806d09c759c059831c1c8ca473f1e1306ae91
gunysunny <mikan31@naver.com>
2025-09-09 19:06:51 +0900

    ♻️ Refactor: calendar.tsx 경로 단축 (#65)

--- src/components/common/date-picker/Calendar.tsx
+++ src/components/common/date-picker/Calendar.tsx
@@ -1,14 +1,14 @@
 import { DayPicker } from 'react-day-picker'
 import 'react-day-picker/style.css'
 import { ko } from 'date-fns/locale'
-import { cn } from '@/utils'
+import { cn } from '@utils'
 
-import type { CalendarProps } from '@/components/common/date-picker/calendar.types'
-import { CALENDAR_CLASSNAMES } from '@/components/common/date-picker/calendar.classnames'
+import type { CalendarProps } from '@components'
+import { CALENDAR_CLASSNAMES } from '@components'
 import {
   buildDisabled,
   buildWrapperClass,
-} from '@/components/common/date-picker/calendar.utils'
+} from '@components/common/date-picker/calendar.utils'
 
 const Calendar = ({
   selected,
