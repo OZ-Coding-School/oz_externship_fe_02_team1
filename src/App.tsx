22fb492d72168ac2b84389672a36580530fb9cce
minji_105 <ajhjmj9@naver.com>
2025-09-11 00:58:58 +0900

    ✨ Feature: 스터디그룹 상세 페이지 라우트 설정 (#79)

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,14 @@ import { Route, Routes } from 'react-router'
 
 import './App.css'
 import { Layout } from '@components'
+import { StudyGroupDetail } from '@pages'
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Layout />}></Route>
+      <Route path="/" element={<Layout />}>
+        <Route path="study-group/:groupId" element={<StudyGroupDetail />} />
+      </Route>
     </Routes>
   )
 }
