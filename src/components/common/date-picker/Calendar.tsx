d10584d6602bbc3074778a69c142ca5ac8f0a031
gunysunny <mikan31@naver.com>
2025-09-11 22:09:32 +0900

    ♻️ Refactor: Calendar.tsx SVG > ChevronIcon 컴포넌트로 분리 (#65)

--- src/components/common/date-picker/Calendar.tsx
+++ src/components/common/date-picker/Calendar.tsx
@@ -9,6 +9,7 @@ import {
   buildDisabled,
   buildWrapperClass,
 } from '@components/common/date-picker/calendar.utils'
+import ChevronIcon from '@assets/icons/chevron'
 
 const Calendar = ({
   selected,
@@ -37,10 +38,7 @@ const Calendar = ({
         className="rdp m-0 w-full rounded-none border-0 bg-transparent p-0 text-gray-900 shadow-none select-none"
         components={{
           Chevron: ({ orientation, className, disabled }) => (
-            <svg
-              width="18"
-              height="18"
-              viewBox="0 0 24 24"
+            <ChevronIcon
               className={cn(
                 'opacity-80 transition',
                 disabled && 'opacity-40',
@@ -49,14 +47,7 @@ const Calendar = ({
                 orientation === 'down' && 'rotate-90',
                 className
               )}
-            >
-              <path
-                d="M9 6l6 6-6 6"
-                stroke="currentColor"
-                strokeWidth="2"
-                fill="none"
-              />
-            </svg>
+            />
           ),
         }}
         classNames={CALENDAR_CLASSNAMES}
