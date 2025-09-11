fa3ea25a0ac0238080e2c4ddb4fc22b0c88cd743
minji_105 <ajhjmj9@naver.com>
2025-09-11 13:20:45 +0900

    🚨 Refactor: vite-env.d.ts 파일의 중복 import 경고 해결 (#79)

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,14 +1,14 @@
 /// <reference types="vite/client" />
 
+import type { FC, SVGProps } from 'react'
+
 declare module '*.svg' {
-  import type { FC, SVGProps } from 'react'
   export const ReactComponent: FC<SVGProps<SVGSVGElement>>
   const src: string
   export default src
 }
 
 declare module '*.svg?react' {
-  import type { FC, SVGProps } from 'react'
   export const ReactComponent: FC<SVGProps<SVGSVGElement>>
   const src: string
   export default src
