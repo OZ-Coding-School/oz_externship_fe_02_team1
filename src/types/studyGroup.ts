ac51b79e5028d9ffb2294859a08538d0131fd2f8
minji_105 <ajhjmj9@naver.com>
2025-09-11 23:08:50 +0900

    🏷️ Types: StudyGroup에 멤버 목록 타입 추가 (#86)

--- src/types/studyGroup.ts
+++ src/types/studyGroup.ts
@@ -5,6 +5,13 @@ export interface StudyGroupLectureList {
   lectureUrl: string
 }
 
+export interface StudyGroupMemberList {
+  id: number
+  name: string
+  profileImage?: string
+  isLeader: boolean
+}
+
 export interface StudyGroup {
   backgroundImage?: string
   studyGroupName: string
@@ -12,5 +19,6 @@ export interface StudyGroup {
   maxMemberCount: number
   startDate: Date
   lastDate: Date
-  lecture?: StudyGroupLectureList[]
+  lecture: StudyGroupLectureList[]
+  member: StudyGroupMemberList[]
 }
