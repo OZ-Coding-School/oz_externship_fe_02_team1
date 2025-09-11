849514870e423c35c5a7b078516189ba5cfcf896
gunysunny <mikan31@naver.com>
2025-09-11 20:23:28 +0900

    ➕ Feature: Chevron 아이콘 컴포넌트(chevron.tsx) 추가 (#65)

---
+++ src/assets/icons/chevron.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react'
+import { cn } from '@/utils'
+
+type Props = React.SVGProps<SVGSVGElement>
+
+export default function ChevronIcon({ className, ...props }: Props) {
+  return (
+    <svg
+      viewBox="0 0 24 24"
+      width={18}
+      height={18}
+      fill="none"
+      stroke="currentColor"
+      strokeWidth={2}
+      aria-hidden
+      className={cn(className)}
+      {...props}
+    >
+      <path d="M9 6l6 6-6 6" strokeLinecap="round" strokeLinejoin="round" />
+    </svg>
+  )
+}
