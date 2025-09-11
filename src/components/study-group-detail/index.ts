87769231677b5c0c8a65618bb92fa8f258adbdcd
minji_105 <ajhjmj9@naver.com>
2025-09-11 18:13:12 +0900

    ♻️ Refactor: study-group-defail/index 파일 업데이트 (#83)

--- src/components/study-group-detail/index.ts
+++ src/components/study-group-detail/index.ts
@@ -1,2 +1,3 @@
 export { default as StudyGroupHeader } from '@components/study-group-detail/StudyGroupHeader'
 export { default as StudyGroupInfo } from '@components/study-group-detail/StudyGroupInfo'
+export { default as StudyGroupLecture } from '@components/study-group-detail/StudyGroupLecture'
