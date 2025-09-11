93f74151b3e42003507f9c15e33257ad2310cceb
minji_105 <ajhjmj9@naver.com>
2025-09-11 18:12:05 +0900

    ✨ Feature: StudyGroupLecture 컴포넌트 추가 (#83)

---
+++ src/components/study-group-detail/StudyGroupLecture.tsx
@@ -0,0 +1,27 @@
+import type { StudyGroup } from '@models'
+import { Card, ImageCard, Text } from '@components'
+import { ArrowUpRightIcon } from '@heroicons/react/24/outline'
+
+export default function StudyGroupLecture({
+  lecture,
+}: Pick<StudyGroup, 'lecture'>) {
+  return (
+    <Card
+      title="스터디 강의"
+      titleVariant="base"
+      className="pb-4 text-lg text-gray-900"
+    >
+      <div className="flex flex-col gap-4">
+        {lecture?.map((el) => (
+          <ImageCard title={el.title} imageUrl="" className="p-4">
+            <Text className="font-medium text-gray-900">{el.title}</Text>
+            <Text className="mt-1 mb-3 text-sm">{el.instructor}</Text>
+            <Text className="text-primary-600 flex cursor-pointer gap-1 text-sm hover:underline">
+              강의 바로가기 <ArrowUpRightIcon width={14} />
+            </Text>
+          </ImageCard>
+        ))}
+      </div>
+    </Card>
+  )
+}
