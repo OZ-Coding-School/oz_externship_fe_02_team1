afc81c7d35a6e7848ebe208ec35fcdcf9be0711d
minji_105 <ajhjmj9@naver.com>
2025-09-11 23:14:30 +0900

    ✨ Feature: StudyGroupDetail 페이지에 멤버 목록 섹션 추가 (#86)

--- src/pages/StudyGroupDetail.tsx
+++ src/pages/StudyGroupDetail.tsx
@@ -3,9 +3,14 @@ import {
   StudyGroupHeader,
   StudyGroupInfo,
   StudyGroupLecture,
+  StudyGroupMember,
 } from '@components'
 
-import type { StudyGroup, StudyGroupLectureList } from '@models'
+import type {
+  StudyGroup,
+  StudyGroupLectureList,
+  StudyGroupMemberList,
+} from '@models'
 
 // 예시 더미 데이터
 const studyGroupLecture: StudyGroupLectureList[] = [
@@ -21,6 +26,49 @@ const studyGroupLecture: StudyGroupLectureList[] = [
     lectureUrl: '/',
   },
 ]
+const studyGroupMember: StudyGroupMemberList[] = [
+  {
+    id: 1,
+    name: '김개발',
+    isLeader: true,
+  },
+  {
+    id: 2,
+    name: '박리액트',
+    isLeader: false,
+  },
+  {
+    id: 3,
+    name: '이프론트',
+    isLeader: false,
+  },
+  {
+    id: 4,
+    name: '최자바',
+    isLeader: false,
+  },
+  {
+    id: 5,
+    name: '한스크립트',
+    isLeader: false,
+  },
+  {
+    id: 6,
+    name: '윤컴포넌트',
+    isLeader: false,
+  },
+  {
+    id: 7,
+    name: '조훅스',
+    isLeader: false,
+  },
+  {
+    id: 8,
+    name: '서스테이트',
+    isLeader: false,
+  },
+]
+
 const studyGroup: StudyGroup = {
   studyGroupName: 'React 실무 프로젝트 스터디',
   currentMemberCount: 8,
@@ -28,6 +76,7 @@ const studyGroup: StudyGroup = {
   startDate: new Date('2024-02-01'),
   lastDate: new Date('2024-04-30'),
   lecture: studyGroupLecture,
+  member: studyGroupMember,
 }
 
 const currentUserRole: 'leader' | 'member' | 'guest' = 'leader'
@@ -57,6 +106,7 @@ export default function StudyGroupDetail() {
           {studyGroup.lecture && (
             <StudyGroupLecture lecture={studyGroup.lecture} />
           )}
+          <StudyGroupMember member={studyGroup.member} />
         </div>
       </div>
     </>
