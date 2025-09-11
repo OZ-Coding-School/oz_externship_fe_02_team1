272a6f4cb2f249c76fa3bbe12f66b67d06a89875
minji_105 <ajhjmj9@naver.com>
2025-09-11 23:39:09 +0900

    ♻️ Refactor: 스터디그룹 더미데이터 파일 분리 (#86)

--- src/pages/StudyGroupDetail.tsx
+++ src/pages/StudyGroupDetail.tsx
@@ -1,86 +1,10 @@
-import { Images } from '@assets'
 import {
   StudyGroupHeader,
   StudyGroupInfo,
   StudyGroupLecture,
   StudyGroupMember,
 } from '@components'
-
-import type {
-  StudyGroup,
-  StudyGroupLectureList,
-  StudyGroupMemberList,
-} from '@models'
-
-// 예시 더미 데이터
-const studyGroupLecture: StudyGroupLectureList[] = [
-  {
-    image: Images.studyGroupDefault,
-    title: 'React 완벽 마스터 강의',
-    instructor: '김개발',
-    lectureUrl: '/',
-  },
-  {
-    title: 'Next.js 실전 가이드',
-    instructor: '박프론트',
-    lectureUrl: '/',
-  },
-]
-const studyGroupMember: StudyGroupMemberList[] = [
-  {
-    id: 1,
-    name: '김개발',
-    isLeader: true,
-  },
-  {
-    id: 2,
-    name: '박리액트',
-    isLeader: false,
-  },
-  {
-    id: 3,
-    name: '이프론트',
-    isLeader: false,
-  },
-  {
-    id: 4,
-    name: '최자바',
-    isLeader: false,
-  },
-  {
-    id: 5,
-    name: '한스크립트',
-    isLeader: false,
-  },
-  {
-    id: 6,
-    name: '윤컴포넌트',
-    isLeader: false,
-  },
-  {
-    id: 7,
-    name: '조훅스',
-    isLeader: false,
-  },
-  {
-    id: 8,
-    name: '서스테이트',
-    isLeader: false,
-  },
-]
-
-const studyGroup: StudyGroup = {
-  studyGroupName: 'React 실무 프로젝트 스터디',
-  currentMemberCount: 8,
-  maxMemberCount: 10,
-  startDate: new Date('2024-02-01'),
-  lastDate: new Date('2024-04-30'),
-  lecture: studyGroupLecture,
-  member: studyGroupMember,
-}
-
-const currentUserRole: 'leader' | 'member' | 'guest' = 'leader'
-const isMember = true
+import { studyGroup, currentUserRole, isMember } from '@mocks/studyGroupDetail'
 
 export default function StudyGroupDetail() {
   return (
