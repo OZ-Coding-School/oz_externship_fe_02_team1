90733592f74317b8eb8d52095648fdb143fe5456
minji_105 <ajhjmj9@naver.com>
2025-09-11 15:20:36 +0900

    🏷️ Types: StudyGroup 타입 정의 추가 (#80)

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,1 +1,1 @@
-// types
+export * from './studyGroup'
---
+++ src/types/studyGroup.ts
@@ -0,0 +1,7 @@
+export interface StudyGroup {
+  studyGroupName?: string
+  currentMemberCount?: number
+  maxMemberCount?: number
+  startDate?: Date
+  lastDate?: Date
+}
