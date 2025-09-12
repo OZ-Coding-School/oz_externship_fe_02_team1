85b4bec65249587ed2c1d3c3f58e202df3f57c57
minji_105 <ajhjmj9@naver.com>
2025-09-12 18:57:59 +0900

    ✨ Feature: StudyGroupLogList에 작성하기 버튼 추가 (#90)

--- src/components/study-group-detail/StudyGroupLogList.tsx
+++ src/components/study-group-detail/StudyGroupLogList.tsx
@@ -1,6 +1,6 @@
-import { PaperClipIcon } from '@heroicons/react/24/outline'
+import { PaperClipIcon, PencilIcon } from '@heroicons/react/24/outline'
 
-import { Avatar, Card, Text } from '@components'
+import { Avatar, Button, Card, Text } from '@components'
 import { formatDate } from '@utils'
 
 import type { StudyGroup } from '@models'
@@ -11,36 +11,41 @@ export default function StudyGroupLogList({
 }: Pick<StudyGroup, 'member' | 'studyLog'>) {
   return (
     <Card
-        title="스터디 기록"
-        titleClassName=" text-xl "
-        cardClassName="flex gap-4"
-      >
-        {studyLog?.map((log) => {
-          const author = member.find((el) => el.id === log.authorId)
+      title="스터디 기록"
+      titleClassName="text-xl mt-1.5 mb-2"
+      cardClassName="flex gap-4"
+    >
+      <Button className="absolute right-[25px] px-4 py-2" size="large">
+        <PencilIcon width={16} />
+        작성하기
+      </Button>
 
-          return (
-            <Card key={log.id} title={log.title} cardClassName="p-[17px]">
-              <Text className="absolute right-[17px] mb-3 text-sm text-gray-500">
-                {formatDate(log.date)}
-              </Text>
+      {studyLog?.map((log) => {
+        const author = member.find((el) => el.id === log.authorId)
 
-              <div className="flex gap-3">
-                <Avatar alt={author?.name || ''} />
-                <div>
-                  <Text className="text-sm font-medium text-gray-700">
-                    {author?.name}
+        return (
+          <Card key={log.id} title={log.title} cardClassName="p-[17px]">
+            <Text className="absolute right-[17px] mb-3 text-sm text-gray-500">
+              {formatDate(log.date)}
+            </Text>
+
+            <div className="flex gap-3">
+              <Avatar alt={author?.name || ''} />
+              <div>
+                <Text className="text-sm font-medium text-gray-700">
+                  {author?.name}
+                </Text>
+                <div className="flex gap-1">
+                  <PaperClipIcon width={12} className="text-gray-500" />
+                  <Text className="text-xs font-medium text-gray-500">
+                    첨부파일 {log.attachment?.length} 개
                   </Text>
-                  <div className="flex gap-1">
-                    <PaperClipIcon width={12} className="text-gray-500" />
-                    <Text className="text-xs font-medium text-gray-500">
-                      첨부파일 {log.attachment?.length} 개
-                    </Text>
-                  </div>
                 </div>
               </div>
-            </Card>
-          )
-        })}
-      </Card>
+            </div>
+          </Card>
+        )
+      })}
+    </Card>
   )
 }
