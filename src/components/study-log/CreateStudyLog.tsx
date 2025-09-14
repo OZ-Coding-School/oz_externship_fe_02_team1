b25056f26184d83c8f78e6ce3864e20c6e3d1fa5
Jun <nbc7799@naver.com>
2025-09-14 16:45:50 +0900

    ✨ Feature: header 섹션 구현 (#61)

--- src/components/study-log/CreateStudyLog.tsx
+++ src/components/study-log/CreateStudyLog.tsx
@@ -1,3 +1,27 @@
+import { H2, Text } from '@components'
+import { BreadCrumb } from '@components'
+
 export default function CreateStudyLog() {
-  return <div>CreateStudyLog</div>
+  const items = [
+    { label: '홈', to: '/' },
+    { label: '스터디 그룹', to: '/study' },
+    { label: '스터디 상세', to: '/study-detail' },
+    { label: '기록 작성' },
+  ]
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault()
+  }
+
+  return (
+    <form onSubmit={handleSubmit} className="mx-48 p-8">
+      <BreadCrumb items={items} className="pb-4" />
+      <header className="pb-6">
+        <H2 className="leading-9 font-bold text-gray-900">스터디 기록 작성</H2>
+        <Text className="inline-flex pt-2 text-gray-600">
+          학습한 내용을 자세히 기록해보세요
+        </Text>
+      </header>
+    </form>
+  )
 }
