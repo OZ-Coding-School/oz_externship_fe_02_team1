7ed594d1ff152d079bda0f52d1317b0703a8b957
minji_105 <ajhjmj9@naver.com>
2025-09-14 13:34:55 +0900

    ♻️ Refactor: 스케줄 캘린더 헤더 로직 분리 (#89)

--- src/components/study-group-detail/calendar/index.ts
+++ src/components/study-group-detail/calendar/index.ts
@@ -1,1 +1,2 @@
 export { default as ScheduleCalendar } from '@components/study-group-detail/calendar/ScheduleCalendar'
+export { default as CalendarHeader } from '@components/study-group-detail/calendar/CalendarHeader'
