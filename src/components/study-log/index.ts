06bb1fde8ff86c6112522f9d4fa87725c332d51d
Jun <nbc7799@naver.com>
2025-09-14 20:36:18 +0900

    ♻️ Refactor: study-log index export 경로 수정 (#61)

--- src/components/study-log/index.ts
+++ src/components/study-log/index.ts
@@ -1,3 +1,3 @@
 export { default as FileUpload } from '@components/study-log/FileUpload'
-export { default as CreateStudyLog } from '@components/study-log/CreateStudyLog'
+export { default as CreateStudyLog } from '@pages/CreateStudyLog'
 export * from '@components/study-log/fileUpload.utils'
