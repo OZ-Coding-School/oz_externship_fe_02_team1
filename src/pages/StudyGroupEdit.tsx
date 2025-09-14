d61615bbdb72219f1cd36ca32e6c8469ac639779
minji_105 <ajhjmj9@naver.com>
2025-09-14 22:57:44 +0900

    ✨ Feature: StudyGroupEdit에 제목 및 버튼 ui 추가 (#118)

--- src/pages/StudyGroupEdit.tsx
+++ src/pages/StudyGroupEdit.tsx
@@ -1,3 +1,30 @@
+import { Button, H2, Text } from '@components'
+import { ArrowLongLeftIcon } from '@heroicons/react/24/outline'
+
 export default function StudyGroupEdit() {
-  return <>StudyGroupEdit</>
+  return (
+    <div className="flex flex-col gap-6 lg:gap-8">
+      <div className="flex items-center gap-4">
+        <button
+          type="button"
+          className="cursor-pointer rounded-full bg-gray-100 p-3 transition-colors duration-300 hover:bg-gray-200"
+        >
+          <ArrowLongLeftIcon width={16} />
+        </button>
+        <div className="flex flex-col gap-1">
+          <H2>스터디 그룹 수정</H2>
+          <Text className="text-gray-600">
+            스터디 그룹 정보를 수정해주세요.
+          </Text>
+        </div>
+      </div>
+
+      <div className="flex justify-end gap-4">
+        <Button variant="outline" className="bg-transparent">
+          취소
+        </Button>
+        <Button>스터디 그룹 만들기</Button>
+      </div>
+    </div>
+  )
 }
