5a1124c188493024ba9dda51c876268a1baeb4e6
Jun <nbc7799@naver.com>
2025-09-14 14:39:37 +0900

    🎉 Begin: 스터디기록 작성 컴포넌트 생성 (#61)

---
+++ src/components/study-log/CreateStudyLog.tsx
@@ -0,0 +1,3 @@
+export default function CreateStudyLog() {
+  return <div>CreateStudyLog</div>
+}
--- src/components/study-log/index.ts
+++ src/components/study-log/index.ts
@@ -1,2 +1,3 @@
-export * from '@components/study-log/FileUpload'
+export { default as FileUpload } from '@components/study-log/FileUpload'
+export { default as CreateStudyLog } from '@components/study-log/CreateStudyLog'
 export * from '@components/study-log/fileUpload.utils'
