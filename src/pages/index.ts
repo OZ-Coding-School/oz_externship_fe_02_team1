4f714b7c953b46936ef01cdfdbe90a3af25faf4f
Jun <nbc7799@naver.com>
2025-09-14 20:35:46 +0900

    ♻️ Refactor: pages index에 스터디기록 작성, 스터디 그룹 생성 export 추가 (#61)

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,2 +1,3 @@
 export { default as StudyGroupDetail } from '@pages/StudyGroupDetail'
 export { default as StudyGroupEdit } from '@pages/StudyGroupEdit'
+export { default as CreateGroup } from '@pages/CreateGroup'
