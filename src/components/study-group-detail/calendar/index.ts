cbb9844b0de96d36949fad17916c863abd91adfe
minji_105 <ajhjmj9@naver.com>
2025-09-14 13:38:52 +0900

    ♻️ Refactor: 스케줄 캘린더 바디 로직 분리 (#89)

--- src/components/study-group-detail/calendar/index.ts
+++ src/components/study-group-detail/calendar/index.ts
@@ -1,3 +1,4 @@
 export { default as ScheduleCalendar } from '@components/study-group-detail/calendar/ScheduleCalendar'
 export { default as CalendarHeader } from '@components/study-group-detail/calendar/CalendarHeader'
 export { default as CalendarDays } from '@components/study-group-detail/calendar/CalendarDays'
+export { default as CalendarBody } from '@components/study-group-detail/calendar/CalendarBody'
