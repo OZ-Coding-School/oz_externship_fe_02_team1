f3973e619510afdb3e14dd1f825c4f6a4aeb88af
minji_105 <ajhjmj9@naver.com>
2025-09-15 01:57:26 +0900

    💄 Design: 멤버 슬라이더 thumb 스타일 수정 (#120)

--- src/components/create-group/MembersSlider.styles.ts
+++ src/components/create-group/MembersSlider.styles.ts
@@ -6,6 +6,6 @@ export const SLIDER_TRACK =
 
 export const SLIDER_THUMB =
   // thumb h-5 vs track h-2 → mt = -(20-8)/2 = -6
-  '[&::-webkit-slider-thumb]:bg-primary-500 [&::-webkit-slider-thumb]:mt-[-6px] [&::-webkit-slider-thumb]:h-5 [&::-webkit-slider-thumb]:w-5 ' +
-  '[&::-webkit-slider-thumb]:appearance-none [&::-webkit-slider-thumb]:rounded-full [&::-webkit-slider-thumb]:border-2 [&::-webkit-slider-thumb]:border-white [&::-webkit-slider-thumb]:shadow ' +
+  '[&::-webkit-slider-thumb]:bg-[#007BFF] [&::-webkit-slider-thumb]:mt-[-4px] [&::-webkit-slider-thumb]:h-4 [&::-webkit-slider-thumb]:w-4 ' +
+  '[&::-webkit-slider-thumb]:appearance-none [&::-webkit-slider-thumb]:rounded-full [&::-webkit-slider-thumb]:shadow ' +
   '[&::-moz-range-thumb]:bg-primary-500 [&::-moz-range-thumb]:h-5 [&::-moz-range-thumb]:w-5 [&::-moz-range-thumb]:rounded-full [&::-moz-range-thumb]:border-2 [&::-moz-range-thumb]:border-white [&::-moz-range-thumb]:shadow'
