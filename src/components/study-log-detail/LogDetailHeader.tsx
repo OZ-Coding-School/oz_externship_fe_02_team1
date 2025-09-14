28463f8233af0a17317485ea8c6399fa96d4475a
Jun <nbc7799@naver.com>
2025-09-15 02:30:56 +0900

    ✨ Feature: 기록상세페이지 헤더부분 UI (#127)

--- src/components/study-log-detail/LogDetailHeader.tsx
+++ src/components/study-log-detail/LogDetailHeader.tsx
@@ -1,3 +1,51 @@
+import { BREAD_CRUMB_PATH } from '@/constants'
+import { BreadCrumb, Button, H3, Text } from '../common'
+
 export default function LogDetailHeader() {
-  return <div>LogDetailHeader</div>
+  const studyLogData = {
+    title: 'React Hooks 실습 정리',
+    userImage: 'https://placehold.co/32x32',
+    userName: '김개발',
+    createdAt: '작성일: 2024. 02. 16. 오전 05:30',
+  }
+
+  return (
+    <header className="w-full max-w-4xl">
+      <BreadCrumb items={BREAD_CRUMB_PATH} className="mb-4" />
+      <div className="flex w-full flex-col p-6">
+        <div className="mb-4 flex w-full justify-between">
+          <H3 className="text-gray-900">{studyLogData.title}</H3>
+          <div>
+            <div className="flex">
+              <Button className="rounded-lg bg-gray-100 px-3 py-1.5">
+                <Text variant="small" className="font-medium text-gray-700">
+                  수정하기
+                </Text>
+              </Button>
+              <Button className="ml-2 rounded-lg bg-red-100 px-3 py-1.5">
+                <Text variant="small" className="font-medium text-red-700">
+                  삭제하기
+                </Text>
+              </Button>
+            </div>
+          </div>
+        </div>
+        <div className="flex items-center">
+          <div className="h-8 w-8 max-w-8">
+            <img src={studyLogData.userImage} alt="" />
+          </div>
+          <Text variant="small" className="ml-2 font-medium text-gray-600">
+            {studyLogData.userName}
+          </Text>
+          <Text variant="small" className="ml-4 font-normal text-gray-600">
+            •
+          </Text>
+          <Text variant="small" className="ml-4 text-gray-600">
+            {studyLogData.createdAt}
+          </Text>
+          <Text>.</Text>
+        </div>
+      </div>
+    </header>
+  )
 }
