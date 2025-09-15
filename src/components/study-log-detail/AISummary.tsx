30efa5f7da2a651386dcd95d14745ac69561eb1b
Jun <nbc7799@naver.com>
2025-09-15 20:35:04 +0900

    ♻️ Refactor: eslint 실행 (#140)

--- src/components/study-log-detail/AISummary.tsx
+++ src/components/study-log-detail/AISummary.tsx
@@ -1,5 +1,6 @@
-import { useEffect, useState } from 'react'
 import axios from 'axios'
+import { useEffect, useState } from 'react'
+
 import { BaseEmptyState, LoadingState } from '@components'
 
 interface AISummaryProps {
