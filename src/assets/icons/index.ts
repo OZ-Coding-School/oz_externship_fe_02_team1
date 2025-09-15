1a57eaec915e00826ff22c96257f0eeedca05d62
Jun <nbc7799@naver.com>
2025-09-15 15:39:44 +0900

    ♻️ Refactor: ai 아이콘 네임 수정 (#128)

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,2 +1,2 @@
 export { default as ImageUploadIcon } from '@assets/icons/image-upload.svg?react'
-export { default as AISummary } from '@assets/icons/ai-summary.svg?react'
+export { default as AIIcon } from '@assets/icons/ai-summary.svg?react'
