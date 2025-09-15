2ce4bfd1d7f7cf623cfd0d3e57b732f30fe56ae8
Jun <nbc7799@naver.com>
2025-09-15 16:08:58 +0900

    ♻️ Refactor: 코드 리팩토링 (#128)

--- src/components/study-log-detail/LogDetailAISummary.tsx
+++ src/components/study-log-detail/LogDetailAISummary.tsx
@@ -1,7 +1,9 @@
-import { AISummary } from '@assets'
-import { Text } from '@components'
-import { EyeIcon, EyeSlashIcon } from '@heroicons/react/24/outline'
 import { useState } from 'react'
+import { EyeIcon, EyeSlashIcon } from '@heroicons/react/24/outline'
+
+import { AIIcon } from '@assets'
+import { Text } from '@components'
+import { AISummary } from '@components'
 
 export default function LogDetailAISummary() {
   const [isSummaryOpened, setIsSummaryOpened] = useState(false)
@@ -10,31 +12,32 @@ export default function LogDetailAISummary() {
     setIsSummaryOpened((prev) => !prev)
   }
 
+  const toggleConfig = isSummaryOpened
+    ? { icon: EyeSlashIcon, label: '접기' }
+    : { icon: EyeIcon, label: '펼치기' }
+
+  const Icon = toggleConfig.icon
+
   return (
-    <section className="flex items-center justify-between p-6">
-      <div className="flex items-center gap-2">
-        <AISummary />
-        <Text variant="large" className="font-semibold text-gray-900">
-          AI 학습 내용 요약
-        </Text>
+    <section className="flex flex-col p-6">
+      <div className="flex items-center justify-between">
+        <div className="flex items-center gap-2">
+          <AIIcon />
+          <Text variant="large" className="font-semibold text-gray-900">
+            AI 학습 내용 요약
+          </Text>
+        </div>
+        <button
+          className="flex cursor-pointer items-center gap-1"
+          onClick={handleClickFold}
+        >
+          <Icon className="h-4 w-4" />
+          <Text variant="small" className="text-gray-600">
+            {toggleConfig.label}
+          </Text>
+        </button>
       </div>
-      <div className="flex items-center gap-1" onClick={handleClickFold}>
-        {isSummaryOpened === true ? (
-          <>
-            <EyeSlashIcon className="h-4 w-4" />
-            <Text variant="small" className="text-gray-600">
-              접기
-            </Text>
-          </>
-        ) : (
-          <>
-            <EyeIcon className="h-4 w-4" />
-            <Text variant="small" className="text-gray-600">
-              펼치기
-            </Text>
-          </>
-        )}
-      </div>
+      {isSummaryOpened && <AISummary />}
     </section>
   )
 }
