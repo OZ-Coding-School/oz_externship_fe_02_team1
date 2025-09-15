9024aaa0b67da951e272e2f87664381a57e9cb25
minji_105 <ajhjmj9@naver.com>
2025-09-15 20:06:49 +0900

    💄 Design: MemberSlider 스타일 일부 수정 (#139)

--- src/components/create-study-group/MembersSlider.styles.ts
+++ src/components/create-study-group/MembersSlider.styles.ts
@@ -8,4 +8,5 @@ export const SLIDER_THUMB_STYLE =
   // thumb h-5 vs track h-2 → mt = -(20-8)/2 = -6
   '[&::-webkit-slider-thumb]:bg-[#007BFF] [&::-webkit-slider-thumb]:mt-[-4px] [&::-webkit-slider-thumb]:h-4 [&::-webkit-slider-thumb]:w-4 ' +
   '[&::-webkit-slider-thumb]:appearance-none [&::-webkit-slider-thumb]:rounded-full [&::-webkit-slider-thumb]:shadow ' +
-  '[&::-moz-range-thumb]:bg-primary-500 [&::-moz-range-thumb]:h-5 [&::-moz-range-thumb]:w-5 [&::-moz-range-thumb]:rounded-full [&::-moz-range-thumb]:border-2 [&::-moz-range-thumb]:border-white [&::-moz-range-thumb]:shadow'
+  '[&::-moz-range-thumb]:bg-primary-500 [&::-moz-range-thumb]:h-5 [&::-moz-range-thumb]:w-5' +
+  ' [&::-moz-range-thumb]:rounded-full [&::-moz-range-thumb]:shadow'
