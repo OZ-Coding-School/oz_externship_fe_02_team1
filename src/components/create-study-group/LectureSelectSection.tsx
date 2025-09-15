97497ebe74fd6e6079e9061338a53312aea7664e
minji_105 <ajhjmj9@naver.com>
2025-09-15 14:00:18 +0900

    ♻️ Refactor: LectureSelectSection 컴포넌트 레이아웃 및 간격 조정 (#121)

--- src/components/create-study-group/LectureSelectSection.tsx
+++ src/components/create-study-group/LectureSelectSection.tsx
@@ -5,32 +5,38 @@ import { studyGroup } from '@mocks/studyGroupDetail'
 
 export default function LectureSelectSection() {
   return (
-    <Card title="강의 선택" titleClassName="text-xl pb-0" cardClassName="p-8">
+    <Card
+      title="강의 선택"
+      titleClassName="text-xl pb-0"
+      cardClassName="p-8 gap-1"
+    >
       <Button className="absolute right-8 py-2 text-base">
         <PlusIcon width={16} />
         강의 추가하기
       </Button>
 
-      <Text className="mt-1 text-gray-600" variant="small">
-        스터디에서 함께 공부할 강의를 선택하세요 (최대 5개)
-      </Text>
+      <div className="flex flex-col gap-4">
+        <Text className="text-gray-600" variant="small">
+          스터디에서 함께 공부할 강의를 선택하세요 (최대 5개)
+        </Text>
 
-      {studyGroup.lecture && studyGroup.lecture.length ? (
-        <div className="mt-4 flex flex-col gap-4 p-3">
-          {studyGroup.lecture.map((el) => (
-            <SelectedLectureCard key={el.title} lecture={el} />
-          ))}
-          <Text variant="small" className="font-medium text-gray-500">
-            {studyGroup.lecture.length}/5개 강의 선택됨
-          </Text>
-        </div>
-      ) : (
-        <div className="py-10 text-center">
-          <Text className="mt-1 text-gray-600" variant="small">
-            선택된 강의가 없습니다.
-          </Text>
-        </div>
-      )}
+        {studyGroup.lecture && studyGroup.lecture.length ? (
+          <div className="flex flex-col gap-4 p-3">
+            {studyGroup.lecture.map((el) => (
+              <SelectedLectureCard key={el.title} lecture={el} />
+            ))}
+            <Text variant="small" className="font-medium text-gray-500">
+              {studyGroup.lecture.length}/5개 강의 선택됨
+            </Text>
+          </div>
+        ) : (
+          <div className="py-10 text-center">
+            <Text className="text-gray-600" variant="small">
+              선택된 강의가 없습니다.
+            </Text>
+          </div>
+        )}
+      </div>
     </Card>
   )
 }
