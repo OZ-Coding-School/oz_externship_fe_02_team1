56b090e235bd1a55f2e63b052ec2641ce5df27a9
minji_105 <ajhjmj9@naver.com>
2025-09-15 14:31:01 +0900

    ✨ Feature: StudyGroupHeader 스터디 수정 버튼 기능 연동 및 버튼 타입 명시 (#117)

--- src/components/study-group-detail/StudyGroupHeader.tsx
+++ src/components/study-group-detail/StudyGroupHeader.tsx
@@ -10,6 +10,7 @@ import { Button, StudyBadge, Text } from '@components'
 import { formatDate } from '@utils'
 
 import type { StudyGroup } from '@models'
+import { usePageNav } from '@hooks'
 
 interface StudyGroupHeaderProps
   extends Pick<
@@ -35,6 +36,8 @@ export default function StudyGroupHeader({
   currentUserRole,
   isMember,
 }: StudyGroupHeaderProps) {
+  const { navigateToGroupEdit } = usePageNav()
+
   const formattedStartDate = formatDate(startDate)
   const formattedLastDate = formatDate(lastDate)
 
@@ -68,13 +71,18 @@ export default function StudyGroupHeader({
 
       <div className="absolute top-6 right-6 flex gap-3">
         {currentUserRole === 'leader' && (
-          <Button variant="ghost" className="bg-white py-2">
+          <Button
+            type="button"
+            variant="ghost"
+            className="bg-white py-2"
+            onClick={navigateToGroupEdit}
+          >
             <PencilIcon width={16} />
             수정하기
           </Button>
         )}
         {isMember && (
-          <Button variant="danger" className="py-2">
+          <Button type="button" variant="danger" className="py-2">
             <ArrowLeftStartOnRectangleIcon width={16} />
             나가기
           </Button>
