3739e50adb0764a02fd12e1a6ea44d819ac01af5
Jun <nbc7799@naver.com>
2025-09-15 18:24:06 +0900

    ✨ Feature: summary 상태 추가 (#140)

--- src/components/study-log-detail/AISummary.tsx
+++ src/components/study-log-detail/AISummary.tsx
@@ -1,3 +1,7 @@
 export default function AISummary() {
-  return <div className="p-4">AI 요약입니다</div>
+  return (
+    <div className="flex flex-col items-start justify-start rounded-lg bg-yellow-50 p-4">
+      AI 요약입니다
+    </div>
+  )
 }
