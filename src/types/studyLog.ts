6206134f4b346ecc88b99f7ecd3ce751fdcc70e8
Jun <nbc7799@naver.com>
2025-09-16 15:30:39 +0900

    ♻️ Refactor: 스터디기록 타입파일 생성 (#129)

---
+++ src/types/studyLog.ts
@@ -0,0 +1,28 @@
+export interface Author {
+  id: number
+  nickName: string
+  profile_image: string
+}
+
+export interface Image {
+  id: number
+  img_url: string
+}
+
+export interface Attachment {
+  id: number
+  file_name: string
+  file_url: string
+}
+
+export interface StudyLog {
+  id: number
+  title: string
+  content: string
+  author: Author
+  images: Image[]
+  attachments: Attachment[]
+  ai_summary: AiSummary
+  created_at: string
+  updated_at: string
+}
