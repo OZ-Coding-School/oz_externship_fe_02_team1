b760bd746edc7c52dc5d2163e110932d9fcab9b2
Jun <nbc7799@naver.com>
2025-09-16 16:49:43 +0900

    ♻️ Refactor: aisummary 삭제 (#129)

--- src/components/study-log-detail/index.ts
+++ src/components/study-log-detail/index.ts
@@ -1,4 +1,3 @@
 export { default as LogDetailHeader } from '@components/study-log-detail/LogDetailHeader'
 export { default as LogDetailAISummary } from '@components/study-log-detail/LogDetailAISummary'
-export { default as AISummary } from '@components/study-log-detail/AISummary'
 export { default as LogDetailMain } from '@components/study-log-detail/LogDetailMain'
