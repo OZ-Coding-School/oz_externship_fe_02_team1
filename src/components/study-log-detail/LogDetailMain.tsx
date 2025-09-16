a192ed5a8a230d9109123cf99dda89b479e5612d
Jun <nbc7799@naver.com>
2025-09-16 17:43:49 +0900

    ✨ Feature: 기존 makdownPreview 사용 (#129)

--- src/components/study-log-detail/LogDetailMain.tsx
+++ src/components/study-log-detail/LogDetailMain.tsx
@@ -1,3 +1,19 @@
-export default function LogDetailMain() {
-  return <div>LogDetailMain</div>
+import { MarkdownPreview } from '@components'
+
+import type { StudyLog } from '@models'
+
+interface LogDetailMainProps {
+  studyLogData: StudyLog
+}
+
+export default function LogDetailMain({ studyLogData }: LogDetailMainProps) {
+  const { content } = studyLogData
+
+  return (
+    <main>
+      <section className="flex flex-col items-start justify-start p-6">
+        <MarkdownPreview value={content} />
+      </section>
+    </main>
+  )
 }
