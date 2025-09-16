999c1e298c7513db03432bacd722c933f496f35e
Jun <nbc7799@naver.com>
2025-09-16 17:45:11 +0900

    ✨ Feature: 더미데이터 사용해서 마크다운 호출 (#129)

--- src/pages/StudyLogDetail.tsx
+++ src/pages/StudyLogDetail.tsx
@@ -1,56 +1,39 @@
-import type { StudyLog } from '@/types/studyLog'
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router'
+
 import {
-  BaseEmptyState,
   LoadingState,
   LogDetailAISummary,
   LogDetailHeader,
   LogDetailMain,
 } from '@components'
-import axios from 'axios'
-import { useEffect, useState } from 'react'
-import { useParams } from 'react-router'
+
+import { dummyStudyLog } from '@/mocks/dummyStudyLog'
+
+import type { StudyLog } from '@models'
 
 export default function StudyLogDetail() {
   const [studyLogData, setStudyLogData] = useState<StudyLog | null>(null)
   const [isLoading, setIsLoading] = useState<boolean>(true)
-  const [error, setError] = useState<string | null>(null)
 
   const { recordId } = useParams()
 
+  // api나오면 더미데이터 교체예정
   useEffect(() => {
     if (!recordId) return
-
-    const fetchStudyLog = async () => {
-      setIsLoading(true)
-      setError(null)
-
-      try {
-        const response = await axios.get(
-          `/api/v1/study-notes/{group.uuid}/notes/{study_notes.id}/`
-        )
-
-        setStudyLogData(response.data)
-      } catch (err) {
-        console.error('API 호출 중 오류 발생', err)
-        setError('스터디 기록을 불러오는 데 실패했습니다.')
-      } finally {
-        setIsLoading(false)
-      }
-    }
-    fetchStudyLog()
+    setIsLoading(true)
+    setStudyLogData(dummyStudyLog)
+    setIsLoading(false)
   }, [recordId])
 
   if (isLoading) {
     return <LoadingState />
   }
 
-  if (error) {
-    return <BaseEmptyState title="에러 발생" description={error} />
+  if (!studyLogData) {
+    return null
   }
 
-  if (!studyLogData) {
-    return
-  }
   return (
     <>
       <LogDetailHeader studyLogData={studyLogData} />
