a193d2a5950f517eb46ebaad21e2c482e4808983
Jun <nbc7799@naver.com>
2025-09-17 17:53:17 +0900

    ♻️ Refactor: 마크다운부분 컴포넌트 분리 (#163)

---
+++ src/components/create-study-log/CreateStudyLogTitle.tsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import { Input, Text } from '@components'
+
+export default function CreateStudyLogTitle() {
+  const [title, setTitle] = useState('')
+
+  return (
+    <div className="w-full">
+      <div className="pb-2">
+        <Text className="text-sm font-medium text-gray-700">제목 </Text>
+        <Text className="text-sm font-medium text-red-500">*</Text>
+      </div>
+      <Input
+        maxLength={99}
+        value={title}
+        placeholder="스터디 기록의 제목을 입력하세요"
+        className="w-full rounded-lg px-3 py-2"
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <Text variant="small" className="pt-1 font-normal text-gray-500">
+        {title.length}/100자
+      </Text>
+    </div>
+  )
+}
