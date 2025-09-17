7e6047941c7451f16f8cd71045226eaa8bccf12a
Jun <nbc7799@naver.com>
2025-09-17 18:21:55 +0900

    ♻️ Refactor: 파일명 수정 log붙임 (#163)

--- src/components/create-study-log/LogUploadPlaceholder.tsx
+++ src/components/create-study-log/LogUploadPlaceholder.tsx
@@ -3,12 +3,12 @@ import { CloudArrowUpIcon } from '@heroicons/react/24/outline'
 import { Text } from '@components'
 import { MAX_FILE_COUNT, MAX_FILE_SIZE_MB } from '@constants'
 
-export const UploadPlaceholder = () => (
+export const LogUploadPlaceholder = () => (
   <label
     htmlFor="file-upload"
     className="flex w-full cursor-pointer flex-col items-center justify-center"
   >
-    <CloudArrowUpIcon className="h-7 w-8 text-gray-400" aria-hidden="true" />{' '}
+    <CloudArrowUpIcon className="h-7 w-8 text-gray-400" aria-hidden="true" />
     <div>
       <Text variant="small" className="pr-1 text-gray-600">
         파일을 여기에 드래그하거나
