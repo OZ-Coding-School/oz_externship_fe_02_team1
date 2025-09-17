508f402e3a5bcd0893a4570ac3a23a6e6cf38a43
Jun <nbc7799@naver.com>
2025-09-18 02:30:37 +0900

    ✨ Feature: 수정 페이지 추가 (#147)

--- src/pages/EditStudyLog.tsx
+++ src/pages/EditStudyLog.tsx
@@ -1,3 +1,31 @@
+import { useState } from 'react'
+
+import {
+  type LogUploadedFile,
+  StudyLogHeader,
+  StudyLogTitle,
+  StudyLogMarkdown,
+  StudyLogFooter,
+  StudyLogLayout,
+} from '@components'
+
 export default function EditStudyLog() {
-  return <div>EditStudyLog</div>
+  const [uploadedFiles, setUploadedFiles] = useState<LogUploadedFile[]>([])
+
+  // todo: api 연결 및 파일 업로드 로직 구현
+  // todo: 기존 스터디 로그 데이터 fetch
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault()
+    return uploadedFiles
+  }
+
+  return (
+    <StudyLogLayout
+      onSubmit={handleSubmit}
+      header={<StudyLogHeader mode="edit" />}
+      title={<StudyLogTitle />}
+      markdown={<StudyLogMarkdown setUploadedFiles={setUploadedFiles} />}
+      footer={<StudyLogFooter />}
+    />
+  )
 }
