7d9df955735c7c58a5bf4c6d90be4954388cb715
minji_105 <ajhjmj9@naver.com>
2025-09-17 17:44:19 +0900

    🚨 Style: import 문 순서 정리 (#157)

--- src/components/study-group/modal/ReviewWriteModal.tsx
+++ src/components/study-group/modal/ReviewWriteModal.tsx
@@ -1,6 +1,5 @@
 import { useForm, Controller } from 'react-hook-form'
-import { studyGroup } from '@mocks/studyGroupDetail'
-import { formatToYMD } from '@utils'
+
 import {
   BaseModal,
   MODAL_PRESETS,
@@ -9,6 +8,8 @@ import {
   Text,
   Textarea,
 } from '@components'
+import { studyGroup } from '@mocks/studyGroupDetail'
+import { formatToYMD } from '@utils'
 
 interface ReviewFormInputs {
   rating: number
