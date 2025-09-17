cb15c293202c9a4077291229f3e817e9f63a5146
Jun <nbc7799@naver.com>
2025-09-18 02:26:34 +0900

    ✨ Feature: 제목이랑 설명을 수정,생성 둘다 이용가능하게 mode 추가 (#147)

---
+++ src/components/study-log/StudyLogHeader.tsx
@@ -0,0 +1,30 @@
+import { BreadCrumb, H2, Text } from '@components'
+import { BREAD_CRUMB_PATH } from '@constants'
+
+type StudyLogHeaderProps = {
+  mode: 'create' | 'edit'
+}
+
+const title = {
+  create: '스터디 기록 작성',
+  edit: '스터디 기록 수정',
+}
+
+const description = {
+  create: '학습한 내용을 자세히 기록해보세요',
+  edit: '수정할 내용을 입력해주세요',
+}
+
+export default function StudyLogHeader({ mode }: StudyLogHeaderProps) {
+  return (
+    <div>
+      <BreadCrumb items={BREAD_CRUMB_PATH} className="pb-4" />
+      <header className="w-full pb-6">
+        <H2 className="leading-9 font-bold text-gray-900">{title[mode]}</H2>
+        <Text className="inline-flex pt-2 text-gray-600">
+          {description[mode]}
+        </Text>
+      </header>
+    </div>
+  )
+}
