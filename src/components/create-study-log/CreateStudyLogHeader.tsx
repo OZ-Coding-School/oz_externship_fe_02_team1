59ef38d55ee01f85c605e666373d3f3240d2c427
Jun <nbc7799@naver.com>
2025-09-17 17:52:44 +0900

    ♻️ Refactor: 스터디기록작성 헤더부분 컴포넌트로 분리 (#163)

---
+++ src/components/create-study-log/CreateStudyLogHeader.tsx
@@ -0,0 +1,16 @@
+import { BREAD_CRUMB_PATH } from '@constants'
+import { BreadCrumb, H2, Text } from '@components'
+
+export default function CreateStudyLogHeader() {
+  return (
+    <div>
+      <BreadCrumb items={BREAD_CRUMB_PATH} className="pb-4" />
+      <header className="w-full pb-6">
+        <H2 className="leading-9 font-bold text-gray-900">스터디 기록 작성</H2>
+        <Text className="inline-flex pt-2 text-gray-600">
+          학습한 내용을 자세히 기록해보세요
+        </Text>
+      </header>
+    </div>
+  )
+}
