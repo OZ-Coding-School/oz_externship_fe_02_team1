1a83e05cf36e485a25262f026f7db91b37e49d75
Jun <nbc7799@naver.com>
2025-09-19 03:24:31 +0900

    ♻️ Refactor: 헤더 공용으로 사용가능하게 리펙토링 (#176)

--- src/components/study-log/StudyLogHeader.tsx
+++ src/components/study-log/StudyLogHeader.tsx
@@ -1,15 +1,30 @@
-import { BreadCrumb, SectionHeader } from '@components'
+import { BreadCrumb, H2, Text } from '@components'
 import { BREAD_CRUMB_PATH } from '@constants'
 
-export default function StudyLogHeader() {
+type StudyLogHeaderProps = {
+  mode: 'create' | 'edit'
+}
+
+const title = {
+  create: '스터디 기록 작성',
+  edit: '스터디 기록 수정',
+}
+
+const description = {
+  create: '학습한 내용을 자세히 기록해보세요',
+  edit: '수정할 내용을 입력해주세요',
+}
+
+export default function StudyLogHeader({ mode }: StudyLogHeaderProps) {
   return (
     <div>
       <BreadCrumb items={BREAD_CRUMB_PATH} className="pb-4" />
-      <SectionHeader
-        title="스터디 기록 작성"
-        subtitle="학습한 내용을 자세히 기록해보세요"
-        titleVariant="3xl"
-      />
+      <header className="w-full pb-6">
+        <H2 className="leading-9 font-bold text-gray-900">{title[mode]}</H2>
+        <Text className="inline-flex pt-2 text-gray-600">
+          {description[mode]}
+        </Text>
+      </header>
     </div>
   )
 }
