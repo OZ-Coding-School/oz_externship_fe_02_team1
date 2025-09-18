1fbb606eec93b61f5891d4bfdc6307ba9bf5ba24
Jun <nbc7799@naver.com>
2025-09-19 03:00:40 +0900

    ♻️ Refactor: import문 정리, placeholder 추가 (#176)

--- src/components/create-study-log/CreateStudyLogMarkdown.tsx
+++ src/components/create-study-log/CreateStudyLogMarkdown.tsx
@@ -1,10 +1,11 @@
 import { useState } from 'react'
 
-import { MarkdownEditor, Text } from '@components'
-
-import LogFileUpload from './LogFileUpload'
-
-import type { LogUploadedFile } from './logFileUpload.utils'
+import {
+  MarkdownEditor,
+  Text,
+  LogFileUpload,
+  type LogUploadedFile,
+} from '@components'
 
 interface SetUploadedFilesProps {
   setUploadedFiles: (files: LogUploadedFile[]) => void
@@ -24,7 +25,11 @@ export default function CreateStudyLogMarkdown({
         <Text className="pb-2 text-sm font-medium text-gray-700">내용 </Text>
         <Text className="text-sm font-medium text-red-500">*</Text>
 
-        <MarkdownEditor value={description} onChange={setDescription} />
+        <MarkdownEditor
+          value={description}
+          onChange={setDescription}
+          placeholder="학습한 내용을 마크다운 형식으로 입력하세요..."
+        />
         <Text className="text-xs font-normal text-gray-500">
           마크다운 문법을 사용할 수 있습니다. 이미지는 드래그 앤 드롭으로 첨부할
           수 있습니다
