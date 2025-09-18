c29c55ab7666d457bd250f28c55c1b36b8d0fd11
minji_105 <ajhjmj9@naver.com>
2025-09-19 02:38:26 +0900

    🏷️ Types: 스케줄 관련 타입 정의 추가 (#175)

--- src/types/schedule.ts
+++ src/types/schedule.ts
@@ -1,8 +1,38 @@
-export interface ScheduleFormInputs {
+import type { User } from '@models'
+
+export interface ScheduleBase {
+  id: number
   title: string
-  goal: string
-  date: Date
-  startTime: string | undefined
-  endTime: string | undefined
-  participants: string[]
+  objective: string
+  session_date: string
+  start_time: string
+  end_time: string
+}
+
+export interface ScheduleCreate extends ScheduleBase {
+  study_group_id: number
+  created_at: string
+}
+
+export interface ScheduleUpdate extends ScheduleBase {
+  study_group_id: number
+  updated_at: string
+}
+
+export interface ScheduleListItem extends ScheduleBase {
+  created_at: string
+}
+
+export type ScheduleList = ScheduleListItem[]
+
+export interface ScheduleParticipant {
+  member_id: number
+  user: (User & {
+    is_leader: boolean
+  })[]
+}
+
+export interface ScheduleDetail extends ScheduleCreate {
+  participants: ScheduleParticipant[]
+  updated_at: string
 }
