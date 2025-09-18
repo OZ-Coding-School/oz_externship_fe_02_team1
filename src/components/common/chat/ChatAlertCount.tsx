96c6564d1059a38d1d0afe019cfa3fd335b3f396
minji_105 <ajhjmj9@naver.com>
2025-09-18 15:04:02 +0900

    ✨ Faeture: ChatAlertCount 컴포넌트 스타일 유연성 추가 (#153)

--- src/components/common/chat/ChatAlertCount.tsx
+++ src/components/common/chat/ChatAlertCount.tsx
@@ -1,13 +1,25 @@
+import { cn } from '@/utils'
 import { Text } from '@components'
 
 interface ChatAlertCountProps {
   alertCount: number
+  isTotalCount?: boolean
 }
 
-export default function ChatAlertCount({ alertCount }: ChatAlertCountProps) {
+export default function ChatAlertCount({
+  alertCount,
+  isTotalCount = false,
+}: ChatAlertCountProps) {
   return (
-    <div className="bg-danger-500 absolute top-[-8px] right-[-8px] flex h-6 w-6 items-center justify-center rounded-full shadow-md">
-      <Text variant="extraSmall" className="font-semibold text-white">
+    <div
+      className={cn(
+        'bg-danger-500 flex items-center justify-center rounded-full text-white',
+        isTotalCount
+          ? 'absolute top-[-8px] right-[-8px] h-6 w-6 shadow-md'
+          : 'h-5 w-5'
+      )}
+    >
+      <Text variant="extraSmall" className="font-semibold">
         {alertCount}
       </Text>
     </div>
