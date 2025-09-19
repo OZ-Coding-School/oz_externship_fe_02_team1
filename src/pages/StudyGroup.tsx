3023cdb06ed1881666a25e55135a9f361be91dad
Jun <nbc7799@naver.com>
2025-09-20 01:19:37 +0900

    ✨ Feature: 스터디그룹에 섹션헤더, 뱃지, 그룹카드 추가 (#179)

--- src/pages/StudyGroup.tsx
+++ src/pages/StudyGroup.tsx
@@ -1,9 +1,23 @@
-import { StudyGroupMainHeader } from '@components'
+import StudyGroupCard from '@/components/study-group/StudyGroupCard'
+import { SectionHeader, StudyBadge, StudyGroupMainHeader } from '@components'
 
 export default function StudyGroup() {
   return (
     <div className="flex flex-col gap-8">
       <StudyGroupMainHeader />
+      <section className="flex flex-col gap-6">
+        <SectionHeader
+          title="진행중인 스터디"
+          subtitle="현재 활발히 진행되고 있는 스터디 그룹들"
+          titleVariant="2xl"
+        >
+          <StudyBadge
+            variant={'inProgress'}
+            className="bg-green-100 text-green-800"
+          />
+        </SectionHeader>
+        <StudyGroupCard />
+      </section>
     </div>
   )
 }
