2ce7a129b2cdf36016b03b536858b8b471f273ba
Jun <nbc7799@naver.com>
2025-09-19 20:42:34 +0900

    ✨ Feature: index에 impor export추가 및 불필요한코드 삭제 (#99)

--- src/components/study-group/index.ts
+++ src/components/study-group/index.ts
@@ -1,2 +1,2 @@
 export * from '@components/study-group/modal'
-export * from '@/components/study-group/StudyGroupMainHeader'
+export { default as StudyGroupMainHeader } from '@components/study-group/StudyGroupMainHeader'
--- src/pages/StudyGroup.tsx
+++ src/pages/StudyGroup.tsx
@@ -1,22 +1,9 @@
-import StudyGroupMainHeader from '@/components/study-group/StudyGroupMainHeader'
-import { SectionHeader, StudyBadge } from '@components'
+import { StudyGroupMainHeader } from '@components'
 
 export default function StudyGroup() {
   return (
     <div className="flex flex-col gap-8">
       <StudyGroupMainHeader />
-      <SectionHeader
-        title="진행중인 스터디"
-        subtitle="현재 활발히 진행되고 있는 스터디 그룹들"
-        titleVariant="2xl"
-      >
-        <StudyBadge
-          variant={'inProgress'}
-          className="bg-green-100 text-green-800"
-        >
-          2개 진행중
-        </StudyBadge>
-      </SectionHeader>
     </div>
   )
 }
