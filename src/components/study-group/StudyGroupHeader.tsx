25ee6ff82e08278df642c3d4282473bf507cac9f
Jun <nbc7799@naver.com>
2025-09-19 14:03:47 +0900

    ✨ Feature: 스터디 그룹페이지 헤더 섹션 컴포넌트 (#99)

--- src/components/study-group/StudyGroupHeader.tsx
+++ src/components/study-group/StudyGroupHeader.tsx
@@ -1,16 +1,30 @@
-import { Input } from '../common'
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline'
-import StudyGroupCard from './StudyGroupCard'
+
+import { Button, Input, SectionHeader, Text } from '@components'
+import { useNavigate } from 'react-router'
 
 export default function StudyGroupHeader() {
+  const navigate = useNavigate()
+
+  const handleCreateNewStudy = () => {
+    navigate('/study-group/create')
+  }
+
   return (
-    <>
-      <div>StudyGroupHeader</div>
+    <header className="flex flex-col gap-8">
+      <SectionHeader
+        title="스터디 그룹"
+        titleVariant="3xl"
+        subtitle="함께 공부하며 성장하는 스터디 그룹에 참여해보세요"
+      >
+        <Button variant="primary" onClick={handleCreateNewStudy}>
+          <Text>+ 새 스터디 만들기</Text>
+        </Button>
+      </SectionHeader>
       <Input
         placeholder="스터디 그룹 검색...."
         leftIcon={<MagnifyingGlassIcon className="h-4 w-4" />}
       />
-      <StudyGroupCard />
-    </>
+    </header>
   )
 }
