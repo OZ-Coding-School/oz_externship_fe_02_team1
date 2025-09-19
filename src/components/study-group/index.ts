9d3a3fa422db49a9cde625107efe392b83c9e245
Jun <nbc7799@naver.com>
2025-09-19 22:40:17 +0900

    ✨ Feature: index 추가 (#179)

--- src/components/study-group/index.ts
+++ src/components/study-group/index.ts
@@ -1,2 +1,3 @@
 export * from '@components/study-group/modal'
 export { default as StudyGroupMainHeader } from '@components/study-group/StudyGroupMainHeader'
+export * from '@components/study-group/StudyGroupCard'
