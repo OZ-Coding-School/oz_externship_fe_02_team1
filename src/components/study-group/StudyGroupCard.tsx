37c8f08774154618da2400afa7f33d955bd3bcd6
Jun <nbc7799@naver.com>
2025-09-20 01:18:44 +0900

    ✨ Feature:  스터디그룹카드 제작 (#179)

--- src/components/study-group/StudyGroupCard.tsx
+++ src/components/study-group/StudyGroupCard.tsx
@@ -1,22 +1,47 @@
+import { BookIcon, CalendarIcon } from '@/assets'
+import { studyGroup } from '@/mocks/studyGroupDetail'
+import { formatToYMD } from '@/utils'
 import { Card, ImageCard, Text } from '@components'
 
-export default function StudyGroupCard() {
+export default function StudyGroupCard({ title }: any) {
+  const { lectures, startAt, endAt } = studyGroup
+
+  const startDate = formatToYMD(new Date(startAt))
+  const endDate = formatToYMD(new Date(endAt))
+
   return (
     <div className="max-w-96">
-      <ImageCard title="React 실무 프로젝트 스터디" />
-      <Card
-        title="React 실무 프로젝트 스터디"
-        cardClassName="flex flex-col gap-3 p-5"
-      >
-        <section className="flex flex-col gap-1">
-          <Text>스터디 기간</Text>
-          <Text>2025년 3월 11일 ~ 2025년 23월 11일</Text>
-        </section>
-        <section className="flex flex-col gap-1">
-          <Text>스터디 강의</Text>
-          <Text>React 완벽 마스터 강의</Text>
-          <Text>Next.js 실전 가이드</Text>
-        </section>
+      <ImageCard title={title} />
+      <Card title={title} cardClassName="flex flex-col gap-3 p-5">
+        <div className="flex flex-col gap-1">
+          <div className="flex items-center gap-2">
+            <CalendarIcon />
+            <Text variant="small" className="font-medium text-gray-600">
+              스터디 기간
+            </Text>
+          </div>
+          <Text variant="small" className="pl-6 font-normal text-gray-700">
+            {startDate} ~ {endDate}
+          </Text>
+        </div>
+        <li className="flex flex-col gap-1">
+          <div className="flex items-center gap-2">
+            <BookIcon />
+            <Text variant="small" className="font-medium text-gray-600">
+              스터디 강의
+            </Text>
+          </div>
+          {lectures.map((lecture) => (
+            <ul key={lecture.id} className="flex flex-col pt-2 pl-5">
+              <Text variant="small" className="font-medium text-gray-600">
+                {lecture.title}
+              </Text>
+              <Text variant="extraSmall" className="font-normal text-gray-500">
+                {lecture.instructor}
+              </Text>
+            </ul>
+          ))}
+        </li>
       </Card>
     </div>
   )
