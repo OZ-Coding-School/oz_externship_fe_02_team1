92f19b7f7cf71c1724d9713ca6c8e095bcbb9db3
minji_105 <ajhjmj9@naver.com>
2025-09-22 04:03:03 +0900

    ♻️ Refactor: ChatMemberList 렌더링 시 key prop 추가 (#154)

--- src/components/common/chat/ChatMemberList.tsx
+++ src/components/common/chat/ChatMemberList.tsx
@@ -11,7 +11,10 @@ export default function ChatMemberList({ members }: ChatMemberListProps) {
   return (
     <div className="scrollbar-custom flex items-center gap-2 overflow-x-scroll overflow-y-hidden border-b border-gray-200 bg-gray-50 p-2">
       {members.map((member) => (
-        <div className="flex items-center gap-1 rounded-full bg-white px-2 py-1">
+        <div
+          key={member.uuid}
+          className="flex items-center gap-1 rounded-full bg-white px-2 py-1"
+        >
           <OnOffIcon isActive={dummyOnlineMembers.includes(member.uuid)} />
           <Text
             variant="extraSmall"
