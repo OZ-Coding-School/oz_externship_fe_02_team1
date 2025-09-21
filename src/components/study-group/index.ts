ea4b9dfb824bf262cbd6c38e24d327f342b55487
Jun <nbc7799@naver.com>
2025-09-22 01:42:50 +0900

    ✨ Feature: footer index 경로 추가 & overlay (#179)

--- src/components/study-group/index.ts
+++ src/components/study-group/index.ts
@@ -1,3 +1,9 @@
 export * from '@components/study-group/modal'
 export { default as StudyGroupMainHeader } from '@components/study-group/StudyGroupMainHeader'
 export { default as StudyGroupCard } from '@components/study-group/StudyGroupCard'
+export {
+  InProgressFooter,
+  CompletedFooter,
+} from '@components/study-group/StudyGroupCardFooters'
+
+export { default as StudyGroupCardOverlay } from '@components/study-group/StudyGroupCardOverlay'
