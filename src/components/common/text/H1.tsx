1c3af423df0d2d1cab207de841decc6cdb0399b1
minji_105 <ajhjmj9@naver.com>
2025-09-21 18:32:45 +0900

    💄 Design: text 컴포넌트에 기본 색상 지정 (#189)

--- src/components/common/text/H1.tsx
+++ src/components/common/text/H1.tsx
@@ -8,7 +8,7 @@ interface H1Props extends ComponentPropsWithoutRef<'h1'> {
 
 export default function H1({ children, className, ...rest }: H1Props) {
   return (
-    <h1 className={cn('text-4xl font-bold', className)} {...rest}>
+    <h1 className={cn('text-4xl font-bold text-gray-900', className)} {...rest}>
       {children}
     </h1>
   )
