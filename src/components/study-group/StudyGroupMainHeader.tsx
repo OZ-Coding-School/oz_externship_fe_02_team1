1b7747eb6c5c3f2d6e33c91d232783d67f885968
Jun <nbc7799@naver.com>
2025-09-22 20:35:04 +0900

    ♻️ Refactor: nav 경로 오타 수정 (#179)

--- src/components/study-group/StudyGroupMainHeader.tsx
+++ src/components/study-group/StudyGroupMainHeader.tsx
@@ -7,7 +7,7 @@ import { usePageNav } from '@hooks'
 export default function StudyGroupMainHeader() {
   const [text, setText] = useState('')
 
-  const { navigateCreateNewStudy } = usePageNav()
+  const { navigateToCreateNewStudy } = usePageNav()
 
   return (
     <header className="flex flex-col gap-8">
@@ -16,7 +16,7 @@ export default function StudyGroupMainHeader() {
         titleVariant="3xl"
         subtitle="함께 공부하며 성장하는 스터디 그룹에 참여해보세요"
       >
-        <Button variant="primary" onClick={navigateCreateNewStudy}>
+        <Button variant="primary" onClick={navigateToCreateNewStudy}>
           <Text>+ 새 스터디 만들기</Text>
         </Button>
       </SectionHeader>
