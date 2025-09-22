6652909b3482af38d8912787bea2304c4161da89
Jun <nbc7799@naver.com>
2025-09-23 01:09:31 +0900

    ♻️ Refactor: 스터디 기간이랑 강의 둘다 body로 합침 (#179)

--- src/components/study-group/StudyGroupCard.tsx
+++ src/components/study-group/StudyGroupCard.tsx
@@ -3,14 +3,15 @@ import {
   ReviewWriteModal,
   Text,
   StudyGroupCardOverlay,
-  StudyGroupCardLectures,
-  StudyGroupCardPeriod,
   StudyGroupCardFooter,
+  StudyGroupCardBody,
 } from '@components'
 import { useModal, usePageNav } from '@hooks'
 
 import type { ReviewFormInputs } from './modal/ReviewWriteModal'
 import type { StudyGroupList } from '@models'
+import { calculateAverageRating } from '@/utils'
+import { studyGroupReview } from '@mocks/studyGroupDetail'
 
 interface StudyGroupCardProps {
   studyGroup: StudyGroupList
@@ -32,42 +33,47 @@ export default function StudyGroupCard({ studyGroup }: StudyGroupCardProps) {
 
   const { navigateToGroupDetail } = usePageNav()
   const { isOpen, openModal, closeModal } = useModal()
+  const averageRating = calculateAverageRating(studyGroupReview)
+  const reviewCount = studyGroupReview.length
 
+  // api연동후 api폴더로 이동예정
   const handleConfirmReview = (data: ReviewFormInputs) => {
     console.log('Review Data:', data)
     closeModal()
   }
-
-  const ImageCardOverLay = (
-    <StudyGroupCardOverlay
-      status={status}
-      isLeader={leader.isLeader}
-      maxHeadcount={maxHeadcount}
-      currentHeadcount={currentHeadcount}
-    />
-  )
 
   return (
     <div className="max-w-96 overflow-hidden">
       <ImageCard
         title={name}
         imageUrl={imgUrl}
-        overlayContent={ImageCardOverLay}
+        overlayContent={
+          <StudyGroupCardOverlay
+            status={status}
+            isLeader={leader.isLeader}
+            maxHeadcount={maxHeadcount}
+            currentHeadcount={currentHeadcount}
+          />
+        }
       >
-        <div className="flex max-w-96 flex-col gap-3 p-5">
+        <div className="flex flex-col gap-3 p-5">
           <Text variant="large" className="font-semibold text-gray-900">
             {name}
           </Text>
-          <StudyGroupCardPeriod startAt={startAt} endAt={endAt} />
-          <StudyGroupCardLectures lectures={lectures} />
+          <StudyGroupCardBody
+            startAt={startAt}
+            endAt={endAt}
+            lectures={lectures}
+          />
           <StudyGroupCardFooter
             status={status}
             navigateToGroupDetail={() => navigateToGroupDetail(uuid)}
             onWriteReview={openModal}
+            averageRating={averageRating}
+            reviewCount={reviewCount}
           />
         </div>
       </ImageCard>
-
       <ReviewWriteModal
         isOpen={isOpen}
         onClose={closeModal}
