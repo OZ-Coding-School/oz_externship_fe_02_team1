1016baec0d8cc16dfdf6a34643d22548a7e18547
Jun <nbc7799@naver.com>
2025-09-22 20:34:41 +0900

    ♻️ Refactor: 스터디그룹카드 기간부분 로직분리 (#179)

---
+++ src/components/study-group/StudyGroupCardPeriod.tsx
@@ -0,0 +1,24 @@
+import { formatToYMD } from '@utils'
+
+import { CalendarIcon } from '@/assets'
+
+import { Text } from '@components'
+
+export default function StudyGroupCardPeriod({ startAt, endAt }: any) {
+  const startDate = formatToYMD(new Date(startAt))
+  const endDate = formatToYMD(new Date(endAt))
+
+  return (
+    <section className="flex flex-col gap-1">
+      <div className="flex items-center gap-2">
+        <CalendarIcon />
+        <Text variant="small" className="font-medium text-gray-600">
+          스터디 기간
+        </Text>
+      </div>
+      <Text variant="small" className="pl-6 font-normal text-gray-700">
+        {startDate} ~ {endDate}
+      </Text>
+    </section>
+  )
+}
