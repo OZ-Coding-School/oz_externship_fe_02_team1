128128d63b155c4ce70105ebbba03cd220108246
minji_105 <ajhjmj9@naver.com>
2025-09-23 01:08:25 +0900

    ♻️ Refactor: StudyGroupForm 컴포넌트 useController로 리팩토링 (#193)

--- src/components/create-study-group/form/StudyGroupForm.tsx
+++ src/components/create-study-group/form/StudyGroupForm.tsx
@@ -1,5 +1,5 @@
 import { PlusIcon } from '@heroicons/react/24/outline'
-import { useFormContext, Controller } from 'react-hook-form'
+import { useFormContext, useController } from 'react-hook-form'
 import { useMediaQuery } from 'react-responsive'
 
 import {
@@ -26,6 +26,19 @@ export default function StudyGroupForm({
   const isMobile = useMediaQuery({ query: mediaQuery.mobile })
   const { handleSubmit, control } = useFormContext<StudyGroupFormValues>()
 
+  const { field: nameField } = useController({ name: 'name', control })
+  const { field: descriptionField } = useController({
+    name: 'introduction',
+    control,
+  })
+  const { field: startAtField } = useController({ name: 'startAt', control })
+  const { field: endAtField } = useController({ name: 'endAt', control })
+  const { field: memberCountField } = useController({
+    name: 'currentHeadcount',
+    control,
+  })
+  const { field: lecturesField } = useController({ name: 'lectures', control })
+
   return (
     <form
       onSubmit={handleSubmit(onSubmit)}
@@ -33,84 +46,48 @@ export default function StudyGroupForm({
     >
       <FormHeader mode={mode} />
 
-      <Controller
-        name="name"
-        control={control}
-        render={({ field: nameField }) => (
-          <Controller
-            name="introduction"
-            control={control}
-            render={({ field: descriptionField }) => (
-              <PrimaryInfoSection
-                groupName={nameField.value}
-                onChangeGroupName={nameField.onChange}
-                description={descriptionField.value}
-                onChangeDescription={descriptionField.onChange}
-                imageFile={null}
-                onChangeImage={() => {}}
-                initialImageUrl={undefined}
-              />
-            )}
-          />
-        )}
+      <PrimaryInfoSection
+        groupName={nameField.value}
+        onChangeGroupName={nameField.onChange}
+        description={descriptionField.value}
+        onChangeDescription={descriptionField.onChange}
+        imageFile={null}
+        onChangeImage={() => {}}
+        initialImageUrl={undefined}
       />
 
-      <Controller
-        name="startAt"
-        control={control}
-        render={({ field: startAtField }) => (
-          <Controller
-            name="endAt"
-            control={control}
-            render={({ field: endAtField }) => (
-              <Controller
-                name="currentHeadcount"
-                control={control}
-                render={({ field: memberCountField }) => (
-                  <PeriodMemberSection
-                    startDate={startAtField.value}
-                    endDate={endAtField.value}
-                    memberCount={memberCountField.value}
-                    onStartDateChange={startAtField.onChange}
-                    onEndDateChange={endAtField.onChange}
-                    onMemberCountChange={memberCountField.onChange}
-                  />
-                )}
-              />
-            )}
-          />
-        )}
+      <PeriodMemberSection
+        startDate={startAtField.value}
+        endDate={endAtField.value}
+        memberCount={memberCountField.value}
+        onStartDateChange={startAtField.onChange}
+        onEndDateChange={endAtField.onChange}
+        onMemberCountChange={memberCountField.onChange}
       />
 
-      <Controller
-        name="lectures"
-        control={control}
-        render={({ field }) => (
-          <LectureSelectSection
-            lectures={field.value}
-            actionSlot={
-              field.value.length < MAX_LECTURES ? (
-                <Button
-                  type="button"
-                  size={isMobile ? 'small' : 'medium'}
-                  className={cn(
-                    'absolute top-6 right-6 py-2',
-                    !isMobile && 'top-8 right-8 text-base'
-                  )}
-                  onClick={() => {
-                    field.onChange([
-                      ...field.value,
-                      { id: Date.now(), title: '새 강의' },
-                    ])
-                  }}
-                >
-                  <PlusIcon width={16} />
-                  강의 추가하기
-                </Button>
-              ) : null
-            }
-          />
-        )}
+      <LectureSelectSection
+        lectures={lecturesField.value}
+        actionSlot={
+          lecturesField.value.length < MAX_LECTURES ? (
+            <Button
+              type="button"
+              size={isMobile ? 'small' : 'medium'}
+              className={cn(
+                'absolute top-6 right-6 py-2',
+                !isMobile && 'top-8 right-8 text-base'
+              )}
+              onClick={() => {
+                lecturesField.onChange([
+                  ...lecturesField.value,
+                  { id: Date.now(), title: '새 강의' },
+                ])
+              }}
+            >
+              <PlusIcon width={16} />
+              강의 추가하기
+            </Button>
+          ) : null
+        }
       />
 
       <FormFooter mode={mode} />
