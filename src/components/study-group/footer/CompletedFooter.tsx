d24db9285b9bf443fdb5043df5982d896727c17a
Jun <nbc7799@naver.com>
2025-09-23 11:07:11 +0900

    ♻️ Refactor: rating icon size 추가 (#179)

--- src/components/study-group/footer/CompletedFooter.tsx
+++ src/components/study-group/footer/CompletedFooter.tsx
@@ -1,5 +1,7 @@
 import { Button, Rating, Text } from '@components'
 
+import { cn } from '@utils'
+
 interface CompletedFooterProps {
   averageRating: number
   reviewCount: number
@@ -19,12 +21,12 @@ export default function CompletedFooter({
   return (
     <div className="flex w-full flex-col justify-end gap-2 border-t border-gray-100 pt-3">
       <div className="flex w-full justify-between">
-        <div className="flex gap-2">
+        <div className="flex items-center gap-2">
           <Text variant="small" className="font-medium text-gray-700">
             스터디 리뷰
           </Text>
           <div className="flex gap-1">
-            <Rating readOnly value={averageRating} />
+            <Rating readOnly value={averageRating} iconSize={16} />
             <Text>
               {averageRating}({reviewCount})
             </Text>
@@ -45,9 +47,9 @@ export default function CompletedFooter({
         type="button"
         variant={hasReview ? 'secondary' : 'primary'}
         onClick={onWriteReview}
-        className={`flex items-center justify-center py-2 ${
-          hasReview ? 'bg-slate-500' : ''
-        }`}
+        className={cn('flex items-center justify-center py-2', {
+          'bg-slate-500': hasReview,
+        })}
       >
         <Text variant="small" className="font-medium text-white">
           {reviewButtonText}
