70997ffcc3d4220f4dbc04464b85c76c0ab54ff1
minji_105 <ajhjmj9@naver.com>
2025-09-24 02:10:24 +0900

    ✨ Feature: QueryClientProvider 적용 (#156)

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { BrowserRouter } from 'react-router'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query' // Import QueryClient and QueryClientProvider
 
 import App from './App.tsx'
 
@@ -12,12 +13,16 @@ async function enableMocking() {
   }
 }
 
+const queryClient = new QueryClient()
+
 enableMocking().then(() => {
   createRoot(document.getElementById('root')!).render(
     <StrictMode>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </QueryClientProvider>
     </StrictMode>
   )
 })
