87f7e1e8cd857cefa5be391b4b358941f24c1d62
minji_105 <ajhjmj9@naver.com>
2025-09-24 04:29:50 +0900

    💡 Comment: useStudyGroupQueries 훅에 todo 주석 추가 (#156)

--- src/hooks/queries/useStudyGroupQueries.ts
+++ src/hooks/queries/useStudyGroupQueries.ts
@@ -12,10 +12,12 @@ export const useStudyGroupQueries = () => {
     >({
       mutationFn: createStudyGroup,
       onSuccess: (response) => {
+        // TODO: 토스트 알림으로 변경
         console.log('Study group created successfully:', response.data)
         alert('스터디 그룹이 성공적으로 생성되었습니다!')
       },
       onError: (error) => {
+        // TODO: 토스트 알림으로 변경
         console.error('Failed to create study group:', error)
         alert('스터디 그룹 생성에 실패했습니다. 다시 시도해주세요.')
       },
