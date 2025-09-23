df5cba56db73e42fbafcd47288070f8e9df0b770
minji_105 <ajhjmj9@naver.com>
2025-09-24 01:31:09 +0900

    🏷️ Types: 스터디그룹 생성 응답 인터페이스 추가 (#156)

---
+++ src/types/api-response-types/studyGroup-response-types.ts
@@ -0,0 +1,12 @@
+// 스터디 그룹 생성 응답
+export interface CreateStudyGroupResponse {
+  uuid: string
+  name: string
+  introduction: string
+  maxHeadcount: number
+  profileImgUrl: string
+  startAt: string
+  endAt: string
+  lectures: number[]
+  createdAt: string
+}
