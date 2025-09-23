eaeab71d1ba76f88095e63b2b6b88aa49a8c0dd8
Jun <nbc7799@naver.com>
2025-09-23 21:44:29 +0900

    ♻️ Refactor: delay 기본값 추가 (#186)

--- src/hooks/useDebounce.ts
+++ src/hooks/useDebounce.ts
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react'
 
-export default function useDebounce<T>(value: T, delay: number): T {
+export default function useDebounce<T>(value: T, delay: 300): T {
   const [debouncedValue, setDebouncedValue] = useState(value)
 
   useEffect(() => {
