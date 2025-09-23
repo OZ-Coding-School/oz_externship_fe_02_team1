ed0f9220e6723534eac380df3314ad9c5bbafb39
Jun <nbc7799@naver.com>
2025-09-23 14:56:13 +0900

    ✨ Feature: useEffect 로직 추가 (#179)

--- src/components/study-group/StudyGroupMainHeader.tsx
+++ src/components/study-group/StudyGroupMainHeader.tsx
@@ -1,13 +1,20 @@
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 import { Button, Input, SectionHeader, Text } from '@components'
 import { usePageNav } from '@hooks'
+import useDebounce from '@/hooks/useDebounce'
 
 export default function StudyGroupMainHeader() {
   const [text, setText] = useState('')
-
+  const debouncedText = useDebounce(text, 500)
   const { navigateToCreateNewStudy } = usePageNav()
+
+  useEffect(() => {
+    if (debouncedText) {
+      console.log('검색 실행', debouncedText)
+    }
+  }, [debouncedText])
 
   return (
     <header className="flex flex-col gap-8">
@@ -17,7 +24,7 @@ export default function StudyGroupMainHeader() {
         subtitle="함께 공부하며 성장하는 스터디 그룹에 참여해보세요"
       >
         <Button variant="primary" onClick={navigateToCreateNewStudy}>
-          <Text>+ 새 스터디 만들기</Text>
+          <Text className="text-white">+ 새 스터디 만들기</Text>
         </Button>
       </SectionHeader>
       <Input
