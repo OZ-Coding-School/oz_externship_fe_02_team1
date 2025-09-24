ac2adece3ab91b097a3c6a7efea1d3257672fafd
minji_105 <ajhjmj9@naver.com>
2025-09-24 17:28:51 +0900

    ♻️ Refactor: 사이드 메뉴 푸터 로직 분리 (#192)

--- src/components/layout/header/side-menu/SideMenu.tsx
+++ src/components/layout/header/side-menu/SideMenu.tsx
@@ -1,18 +1,11 @@
 import {
-  ArrowLeftStartOnRectangleIcon,
   BookOpenIcon,
   MegaphoneIcon,
   UserGroupIcon,
   UserIcon,
 } from '@heroicons/react/24/outline'
 
-import {
-  AuthButtonGroup,
-  Avatar,
-  Button,
-  SideMenuHeader,
-  Text,
-} from '@components'
+import { Button, SideMenuFooter, SideMenuHeader, Text } from '@components'
 import { cn } from '@utils'
 import { HEADER_MOBILE_NAV_LISTS } from '@constants'
 import { Link, useLocation } from 'react-router'
@@ -83,31 +76,7 @@ export default function HeaderSideMenu({
           </div>
         </div>
 
-        <footer className="border-t border-gray-200 p-4">
-          {isLoggedin ? (
-            <div className="flex flex-col gap-4">
-              <div className="flex items-center gap-3">
-                <Avatar src="none" alt="김개발" size="md" />
-                <div className="flex flex-col">
-                  <Text className="font-semibold">김 개발</Text>
-                  <Text variant="extraSmall" className="text-gray-600">
-                    kim.dev@example.com
-                  </Text>
-                </div>
-              </div>
-              <Button
-                variant="secondary"
-                size="large"
-                className="justify-center px-4 py-2"
-              >
-                <ArrowLeftStartOnRectangleIcon width={18} className="h-7" />
-                로그아웃
-              </Button>
-            </div>
-          ) : (
-            <AuthButtonGroup />
-          )}
-        </footer>
+        <SideMenuFooter isLoggedin={isLoggedin} />
       </div>
     </div>
   )
