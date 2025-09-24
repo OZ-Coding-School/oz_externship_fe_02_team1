4bf131d57df39cc2c2bf20c6b86d23ee2a83827f
minji_105 <ajhjmj9@naver.com>
2025-09-24 14:37:30 +0900

    ♻️ Refactor: UserDropdown 메뉴 항목 직접 렌더링하도록 변경 (#192)

--- src/components/layout/header/UserDropdown.tsx
+++ src/components/layout/header/UserDropdown.tsx
@@ -1,4 +1,8 @@
-import { UserMenu } from '@components'
+import { Button } from '@components'
+import {
+  ArrowLeftStartOnRectangleIcon,
+  UserIcon,
+} from '@heroicons/react/24/outline'
 
 interface UserDropdownProps {
   isOpen: boolean
@@ -9,8 +13,25 @@ export default function UserDropdown({ isOpen }: UserDropdownProps) {
 
   return (
     <div className="relative">
-      <div className="absolute top-6 right-0 flex w-48 flex-col rounded-lg border border-gray-200 bg-white py-2 shadow-md">
-        <UserMenu />
+      <div className="absolute top-2 right-0 flex w-48 flex-col rounded-lg border border-gray-200 bg-white py-2 shadow-md">
+        <div className="px-2">
+          <Button variant="ghost" className="w-full gap-3 p-2">
+            <UserIcon width={14} />
+            마이페이지
+          </Button>
+        </div>
+        <div className="py-1">
+          <div className="border border-gray-100" />
+        </div>
+        <div className="px-2">
+          <Button variant="ghost" className="text-danger-600 w-full gap-3 p-2">
+            <ArrowLeftStartOnRectangleIcon
+              width={14}
+              className="text-danger-600"
+            />
+            로그아웃
+          </Button>
+        </div>
       </div>
     </div>
   )
