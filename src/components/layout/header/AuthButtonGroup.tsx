a4765f6b8e09ddffd828248e862fa7c16e75a9b5
minji_105 <ajhjmj9@naver.com>
2025-09-24 17:15:29 +0900

    💄 Design: AuthButtonGroup 반응형 레이아웃 개선 (#192)

--- src/components/layout/header/AuthButtonGroup.tsx
+++ src/components/layout/header/AuthButtonGroup.tsx
@@ -1,22 +1,20 @@
-import { useMediaQuery } from 'react-responsive'
-
 import { Button } from '@components'
-import { mediaQuery } from '@constants'
-import { cn } from '@utils'
 
 interface AuthButtonGroupProps {
   onClose?: () => void
 }
 
 export default function AuthButtonGroup({ onClose }: AuthButtonGroupProps) {
-  const isMobile = useMediaQuery({ query: mediaQuery.mobile })
-
   return (
-    <div className={cn('flex gap-2', isMobile && 'flex-col')}>
-      <Button variant="ghost" className="text-base" onClick={onClose}>
+    <div className="flex gap-2">
+      <Button
+        variant="ghost"
+        className="flex-grow justify-center text-base"
+        onClick={onClose}
+      >
         로그인
       </Button>
-      <Button className="text-base" onClick={onClose}>
+      <Button className="flex-grow justify-center text-base" onClick={onClose}>
         회원가입
       </Button>
     </div>
