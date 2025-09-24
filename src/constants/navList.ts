4d6f12ef5b65f34f0e65d80e9d92e7b78771f889
minji_105 <ajhjmj9@naver.com>
2025-09-24 16:36:23 +0900

    ✨ Feature: 모바일 사이드 메뉴 nav 리스트 상수 추가 (#192)

--- src/constants/navList.ts
+++ src/constants/navList.ts
@@ -4,6 +4,13 @@ export const HEADER_DESKTOP_NAV_LISTS = [
   { name: '구인 공고', path: '/recruitment' },
 ]
 
+export const HEADER_MOBILE_NAV_LISTS = [
+  { name: '강의', path: '/courses' },
+  { name: '그룹', path: '/study-group' },
+  { name: '공고', path: '/recruitment' },
+  { name: '내정보', path: '/my-page' },
+]
+
 export const FOOTER_NAV_LISTS = [
   {
     title: '서비스',
