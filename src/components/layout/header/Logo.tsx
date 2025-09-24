3d89990531d853d6df48f626a7cfc8b321edfff6
minji_105 <ajhjmj9@naver.com>
2025-09-24 16:35:45 +0900

    ✨ Feature: Logo 컴포넌트에 size prop 추가 (#192)

--- src/components/layout/header/Logo.tsx
+++ src/components/layout/header/Logo.tsx
@@ -1,9 +1,24 @@
+import { cn } from '@utils'
 import { Text } from '@components'
 
-export default function Logo() {
+interface LogoProps {
+  size?: 'md' | 'lg'
+}
+
+export default function Logo({ size = 'md' }: LogoProps) {
   return (
-    <div className="bg-primary-500 flex h-8 w-8 items-center justify-center rounded-lg">
-      <Text className="font-bold text-white">S</Text>
+    <div
+      className={cn(
+        'bg-primary-500 flex items-center justify-center rounded-lg',
+        size === 'md' ? 'h-8 w-8' : 'h-10 w-10'
+      )}
+    >
+      <Text
+        variant={size === 'md' ? 'base' : 'large'}
+        className="font-bold text-white"
+      >
+        S
+      </Text>
     </div>
   )
 }
