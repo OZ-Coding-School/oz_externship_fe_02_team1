15621a6892b30834d330b75a7fd135f143042108
minji_105 <ajhjmj9@naver.com>
2025-09-26 02:39:30 +0900

    ✨ Feature: 스터디그룹 상세 조회 API 연동 (#212)

--- src/pages/StudyGroupDetail.tsx
+++ src/pages/StudyGroupDetail.tsx
@@ -1,4 +1,6 @@
+import { useParams } from 'react-router-dom'
 import {
+  LoadingState,
   StudyGroupHeader,
   StudyGroupInfo,
   StudyGroupLecture,
@@ -6,41 +8,58 @@ import {
   StudyGroupMember,
   StudyGroupSchedule,
 } from '@components'
-import {
-  studyGroup,
-  studyGroupLog,
-  studyGroupSchedule,
-} from '@mocks/datas/studyGroupDetail'
+import { useStudyGroupDetail } from '@hooks'
 
 export default function StudyGroupDetail() {
+  const { groupId } = useParams<{ groupId: string }>()
+
+  const { data: studyGroupData, isLoading } = useStudyGroupDetail(groupId || '')
+
+  if (isLoading) {
+    return <LoadingState />
+  }
+
+  if (!studyGroupData) {
+    // TODO: 404 컴포넌트
+    return <div>스터디 그룹을 찾을 수 없습니다.</div>
+  }
+
+  const {
+    name,
+    current_headcount: currentHeadcount,
+    max_headcount: maxHeadcount,
+    start_at: startAt,
+    end_at: endAt,
+    members,
+    lectures,
+  } = studyGroupData
+
+  const studyGroupLog: any[] = []
+  const studyGroupSchedule: any[] = []
+
   return (
     <>
       <StudyGroupHeader
-        name={studyGroup.name}
-        currentHeadcount={studyGroup.currentHeadcount}
-        maxHeadcount={studyGroup.maxHeadcount}
-        startAt={studyGroup.startAt}
-        endAt={studyGroup.endAt}
+        name={name}
+        currentHeadcount={currentHeadcount}
+        maxHeadcount={maxHeadcount}
+        startAt={startAt}
+        endAt={endAt}
       />
       <div className="mt-6 flex flex-col gap-6 lg:mt-8 lg:grid lg:grid-cols-3">
         <div className="col-span-2 flex flex-col gap-6 lg:gap-8">
           <StudyGroupSchedule schedule={studyGroupSchedule} />
-          <StudyGroupLogList
-            member={studyGroup.members}
-            studyLog={studyGroupLog}
-          />
+          <StudyGroupLogList member={members} studyLog={studyGroupLog} />
         </div>
         <div className="flex flex-col gap-6">
           <StudyGroupInfo
-            currentHeadcount={studyGroup.currentHeadcount}
-            maxHeadcount={studyGroup.maxHeadcount}
-            startAt={studyGroup.startAt}
-            endAt={studyGroup.endAt}
+            currentHeadcount={currentHeadcount}
+            maxHeadcount={maxHeadcount}
+            startAt={startAt}
+            endAt={endAt}
           />
-          {studyGroup.lectures && (
-            <StudyGroupLecture lectures={studyGroup.lectures} />
-          )}
-          <StudyGroupMember members={studyGroup.members} />
+          {lectures && <StudyGroupLecture lectures={lectures} />}
+          <StudyGroupMember members={members} />
         </div>
       </div>
     </>
