5a623570aedc5f85f099c2b1116d3d273781b5eb
minji_105 <ajhjmj9@naver.com>
2025-09-26 13:57:19 +0900

    ♻️ Refactor: 채팅 목록 데이터 로딩 중 ui 개선 (#218)

--- src/components/common/chat/ChatRoomList.tsx
+++ src/components/common/chat/ChatRoomList.tsx
@@ -18,42 +18,43 @@ export default function ChatRoomList({
   onToggle,
   onSelectChat,
 }: ChatRoomListProps) {
-  if (isLoading) {
-    return <LoadingState />
-  }
-
   return (
     <ChatContainer
       header={
         <div className="flex flex-col items-start">
           <Text className="font-semibold">채팅방</Text>
           <Text variant="extraSmall" className="text-primary-600">
-            {totalUnreadCount}개의 읽지 않은 메시지
+            {isLoading ? 0 : totalUnreadCount}개의 읽지 않은 메시지
           </Text>
         </div>
       }
       onToggle={onToggle}
     >
-      <div className="scrollbar-hidden h-77 overflow-x-hidden overflow-y-scroll">
-        {chatRoomList && chatRoomList.length > 0 ? (
-          chatRoomList.map((chat, index) => (
-            <div
-              key={chat.uuid}
-              onClick={() => onSelectChat(chat)}
-              className={cn(
-                'flex cursor-pointer flex-col gap-1 p-3',
-                index !== chatRoomList.length - 1 && 'border-b border-gray-200'
-              )}
-            >
-              <ChatPreview chat={chat} />
+      {isLoading ? (
+        <LoadingState />
+      ) : (
+        <div className="scrollbar-hidden h-77 overflow-x-hidden overflow-y-scroll">
+          {chatRoomList && chatRoomList.length > 0 ? (
+            chatRoomList.map((chat, index) => (
+              <div
+                key={chat.uuid}
+                onClick={() => onSelectChat(chat)}
+                className={cn(
+                  'flex cursor-pointer flex-col gap-1 p-3',
+                  index !== chatRoomList.length - 1 &&
+                    'border-b border-gray-200'
+                )}
+              >
+                <ChatPreview chat={chat} />
+              </div>
+            ))
+          ) : (
+            <div className="flex h-full items-center justify-center">
+              <Text className="text-gray-500">채팅방이 없습니다.</Text>
             </div>
-          ))
-        ) : (
-          <div className="flex h-full items-center justify-center">
-            <Text className="text-gray-500">채팅방이 없습니다.</Text>
-          </div>
-        )}
-      </div>
+          )}
+        </div>
+      )}
     </ChatContainer>
   )
 }
