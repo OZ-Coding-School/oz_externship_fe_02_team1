02754593d18af60d40bf25c0257764406f3c2fb9
minji_105 <ajhjmj9@naver.com>
2025-09-26 11:09:02 +0900

    ✨ Feature: 채팅 목록 조회 api 호출 메서드 추가 (#218)

---
+++ src/api/chat.ts
@@ -0,0 +1,12 @@
+import { API_PATHS } from '@constants'
+import { axiosInstance } from '@lib'
+import type { ChatRoomsResponse } from './types'
+
+export const chatApi = {
+  getChatRoomList: async () => {
+    const response = await axiosInstance.get<ChatRoomsResponse>(
+      API_PATHS.CHAT.ROOMS
+    )
+    return response.data
+  },
+}
--- src/api/index.ts
+++ src/api/index.ts
@@ -1,3 +1,4 @@
 export * from '@api/group'
 export * from '@api/auth'
+export * from '@api/chat'
 export * from '@api/types'
