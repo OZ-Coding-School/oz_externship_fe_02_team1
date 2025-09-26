f94352016e31c42cb2a87d22339f648076dbeef3
minji_105 <ajhjmj9@naver.com>
2025-09-26 15:14:58 +0900

    ♻️ Refactor: useChatRoomsList 쿼리 훅에 enabled 추가 (#218)

--- src/hooks/queries/useChatRoomsList.ts
+++ src/hooks/queries/useChatRoomsList.ts
@@ -2,10 +2,14 @@ import { useQuery } from '@tanstack/react-query'
 
 import { chatApi } from '@api'
 import { chatQueryKey } from '@hooks'
+import { useAuthStore } from '@store'
 
 export const useChatRoomsList = () => {
+  const { isLoggedIn } = useAuthStore()
+
   return useQuery({
     queryKey: chatQueryKey.list(),
     queryFn: () => chatApi.getChatRoomList(),
+    enabled: !!isLoggedIn,
   })
 }
