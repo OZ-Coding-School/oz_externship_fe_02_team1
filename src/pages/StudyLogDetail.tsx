7fd974ce9f230d9294c514ac276eb6bb6ade440b
Jun <nbc7799@naver.com>
2025-09-29 04:20:01 +0900

    ♻️ Refactor: build에러 수정 (#227)

--- src/pages/StudyLogDetail.tsx
+++ src/pages/StudyLogDetail.tsx
@@ -10,6 +10,7 @@ import {
 } from '@components'
 import { BREAD_CRUMB_PATH } from '@constants'
 import { useStudyLogQuery } from '@hooks'
+import type { StudyLogDetail } from '@/types'
 
 export default function StudyLogDetail() {
   const { groupId, recordId } = useParams<{
@@ -21,6 +22,15 @@ export default function StudyLogDetail() {
   const noteId = Number(recordId)
 
   const { data: studyLogData, isLoading } = useStudyLogQuery(groupId!, noteId)
+
+  // API 응답 데이터를 프론트엔드 컴포넌트에서 사용하는 데이터 형식으로 변환합니다.
+  const studyLogDetail: StudyLogDetail | undefined = useMemo(() => {
+    if (!studyLogData) return undefined
+    return {
+      ...studyLogData,
+      studyGroupId: String(studyLogData.studyGroup),
+    }
+  }, [studyLogData])
 
   const breadCrumbPath = useMemo(() => {
     if (!groupId) return BREAD_CRUMB_PATH
@@ -34,7 +44,7 @@ export default function StudyLogDetail() {
     return <LoadingState />
   }
 
-  if (!studyLogData) {
+  if (!studyLogDetail) {
     // TODO: 404 컴포넌트 또는 에러 메시지
     return <div>스터디 기록을 찾을 수 없습니다.</div>
   }
@@ -43,11 +53,11 @@ export default function StudyLogDetail() {
     <div className="flex flex-col gap-4">
       <BreadCrumb items={breadCrumbPath} />
       <div>
-        <LogDetailHeader studyLogData={studyLogData} />
-        {studyLogData.aiSummary && (
-          <LogDetailAISummary summaryText={studyLogData.aiSummary} />
+        <LogDetailHeader studyLogData={studyLogDetail} />
+        {studyLogDetail.aiSummary && (
+          <LogDetailAISummary summaryText={studyLogDetail.aiSummary} />
         )}
-        <LogDetailMain studyLogData={studyLogData} />
+        <LogDetailMain studyLogData={studyLogDetail} />
       </div>
       <Link
         to={'/study-group'}
