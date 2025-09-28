d6aefb7c2775e7efc63ba269985d5f7d3f74a3d5
minji_105 <ajhjmj9@naver.com>
2025-09-29 01:14:10 +0900

    🚚 Rename: 기록 상세 조회 쿼리 훅 이름 변경 (#231)

--- src/hooks/queries/useLogDetailQuery.ts
+++ src/hooks/queries/useLogDetailQuery.ts
@@ -3,7 +3,7 @@ import { useQuery, type UseQueryOptions } from '@tanstack/react-query'
 import { logApi, type StudyLogDetailResponse } from '@api'
 import { logDetailKey } from '@hooks'
 
-export const useStudyLogQuery = (
+export const useLogDetailQuery = (
   groupUuid: string,
   noteId: number,
   options?: Omit<
