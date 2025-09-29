3abf7293fd71d83d76c734fcabe5f1dff39be989
Jun <nbc7799@naver.com>
2025-09-29 21:10:37 +0900

    ♻️ Refactor: 디테일 페이지최상위에 있던 버튼들 삭제 및 response 타입 변경 (#238)

--- src/pages/StudyLogDetail.tsx
+++ src/pages/StudyLogDetail.tsx
@@ -3,7 +3,6 @@ import { Link, useNavigate, useParams } from 'react-router-dom'
 
 import {
   BreadCrumb,
-  Button,
   LoadingState,
   LogDetailAISummary,
   LogDetailHeader,
@@ -11,10 +10,9 @@ import {
 } from '@components'
 import { BREAD_CRUMB_PATH } from '@constants'
 
-import type { StudyLogDetail } from '@models'
 import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { useLogDetailQuery } from '@hooks'
-import { logApi } from '@/api'
+import { logApi, type StudyLogDetailResponse } from '@/api'
 import { useAuthStore } from '@/store'
 
 export default function StudyLogDetail() {
@@ -23,13 +21,11 @@ export default function StudyLogDetail() {
     recordId: string
   }>()
 
-  // recordId를 숫자로 변환
   const noteId = Number(recordId)
 
   const navigate = useNavigate()
   const queryClient = useQueryClient()
 
-  // Zustand 스토어에서 현재 로그인한 사용자 정보 가져오기
   const currentUser = useAuthStore((state) => state.user)
   const { data: studyLogData, isLoading } = useLogDetailQuery(groupId!, noteId)
 
@@ -41,7 +37,6 @@ export default function StudyLogDetail() {
       queryClient.invalidateQueries({ queryKey: ['studyLogs', groupId] })
       // 현재 상세 페이지 캐시 제거
       queryClient.removeQueries({ queryKey: ['studyLog', groupId, noteId] })
-      // 목록 페이지로 이동
       navigate(`/study-group/${groupId}`)
     },
     onError: () => {
@@ -50,7 +45,7 @@ export default function StudyLogDetail() {
   })
 
   // API 응답 데이터를 프론트엔드 컴포넌트에서 사용하는 데이터 형식으로 변환합니다.
-  const studyLogDetail: StudyLogDetail | undefined = useMemo(() => {
+  const studyLogDetail: StudyLogDetailResponse | undefined = useMemo(() => {
     if (!studyLogData) return undefined
     return {
       ...studyLogData,
@@ -58,11 +53,7 @@ export default function StudyLogDetail() {
     }
   }, [studyLogData])
 
-  // 현재 사용자가 글 작성자인지 확인
-  const isAuthor = useMemo(
-    () => currentUser?.uuid === studyLogDetail?.author.id,
-    [currentUser, studyLogDetail]
-  )
+  const isAuthor = currentUser?.id === studyLogDetail?.author.id
 
   const breadCrumbPath = useMemo(() => {
     if (!groupId) return BREAD_CRUMB_PATH
@@ -73,6 +64,7 @@ export default function StudyLogDetail() {
   }, [groupId])
 
   const handleDelete = () => {
+    console.log('삭제입니다')
     if (window.confirm('정말로 이 기록을 삭제하시겠습니까?')) {
       deleteLog()
     }
@@ -87,31 +79,23 @@ export default function StudyLogDetail() {
     return <div>스터디 기록을 찾을 수 없습니다.</div>
   }
 
+  const handleEdit = () =>
+    navigate(`/study-group/${groupId}/records/${noteId}/edit`)
+
   return (
     <div className="flex flex-col gap-4">
       <BreadCrumb items={breadCrumbPath} />
       <div>
-        <LogDetailHeader studyLogData={studyLogDetail} />
+        <LogDetailHeader
+          studyLogData={studyLogDetail}
+          isAuthor={isAuthor}
+          onEdit={handleEdit}
+          onDelete={handleDelete}
+        />
         {studyLogDetail.aiSummary && (
           <LogDetailAISummary summaryText={studyLogDetail.aiSummary} />
         )}
         <LogDetailMain studyLogData={studyLogDetail} />
-
-        {isAuthor && (
-          <div className="mt-4 flex justify-end gap-2">
-            <Button
-              variant="secondary"
-              onClick={() =>
-                navigate(`/study-group/${groupId}/log/${noteId}/edit`)
-              }
-            >
-              수정하기
-            </Button>
-            <Button variant="danger" onClick={handleDelete}>
-              삭제하기
-            </Button>
-          </div>
-        )}
       </div>
       <Link
         to={'/study-group'}
