f66406c4874d23a21345cfef6613052973483bf3
minji_105 <ajhjmj9@naver.com>
2025-10-01 01:03:45 +0900

    💄 Design: 토스트 z-index 추가 (#201)

--- src/components/common/toast/ToastProvider.tsx
+++ src/components/common/toast/ToastProvider.tsx
@@ -6,7 +6,7 @@ export default function ToastProvider() {
   const { toasts, removeToast } = useToastStore()
 
   return (
-    <div className="fixed top-4 right-4 z-50 w-full max-w-xs space-y-2">
+    <div className="fixed top-4 right-4 z-50 z-9999 w-full max-w-xs space-y-2">
       {toasts.map((toast) => (
         <Toast
           key={toast.id}
