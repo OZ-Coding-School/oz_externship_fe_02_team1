5064a092c2e6a736bd55d6492f794dbdf9d7c832
minji_105 <ajhjmj9@naver.com>
2025-10-01 01:20:15 +0900

    ✨ Feature: 멤버 추방 성공/실패 시 토스트 알림 (#201)

--- src/hooks/queries/useKickMemberMutation.ts
+++ src/hooks/queries/useKickMemberMutation.ts
@@ -1,23 +1,32 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query'
 
 import { studyApi } from '@api'
-import { studyQueryKey } from '@hooks'
+import { studyQueryKey, useToast } from '@hooks'
 
 export const useKickMemberMutation = (groupUuid: string) => {
   const queryClient = useQueryClient()
+  const { toast } = useToast()
 
   return useMutation({
     mutationFn: (memberUuid: string) =>
       studyApi.kickGroupMember(groupUuid, memberUuid),
     onSuccess: () => {
-      alert('멤버를 내보냈습니다.')
+      toast({
+        title: '멤버를 내보냈습니다.',
+        message: '',
+        type: 'success',
+      })
 
       return queryClient.invalidateQueries({
         queryKey: studyQueryKey.detail(groupUuid),
       })
     },
     onError: () => {
-      alert('멤버를 내보내는 데 실패했습니다.')
+      toast({
+        title: '멤버를 내보내는 데 실패했습니다.',
+        message: '다시 시도해주세요.',
+        type: 'error',
+      })
     },
   })
 }
